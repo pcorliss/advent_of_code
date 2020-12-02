396c077b1efa7bc401156fa283dffad1ddd81395
Philip Corliss <pcorliss@bigdevbox.com>
2020-12-02 16:36:19 +0000

    New password scheme same input

--- 02/password.rb
+++ 02/password.rb
@@ -26,4 +26,27 @@ module Advent
       end
     end
   end
+
+  class TwoPartTwo
+    attr_reader :input
+
+    def initialize(input)
+      @input = Set.new
+      input.each_line do |line|
+        if line =~ /(\d+)-(\d+)\s(.): (.*)$/
+          @input.add [$1.to_i, $2.to_i, $3, $4]
+        end
+      end
+    end
+
+    def valid_password?(valid_idx, invalid_idx, char, password)
+      password[valid_idx-1] == char && password[invalid_idx-1] != char
+    end
+
+    def valid_password_count
+      @input.count do |e|
+        valid_password?(*e)
+      end
+    end
+  end
 end
--- 02/spec/password_spec.rb
+++ 02/spec/password_spec.rb
@@ -1,7 +1,7 @@
 require '../password.rb'
 require 'rspec'
 
-describe Advent do
+describe Advent::Two do
   let(:input) {
     <<~EOS
       1-3 a: abcde
@@ -46,6 +46,59 @@ describe Advent do
   describe "#valid_password_count" do
     it "returns a count of valid passwords" do
       expect(ad.valid_password_count).to eq(2)
+    end
+  end
+end
+
+describe Advent::TwoPartTwo do
+  let(:input) {
+    <<~EOS
+      1-3 a: abcde
+      1-3 b: cdefg
+      2-9 c: ccccccccc
+    EOS
+  }
+  let(:ad) { Advent::TwoPartTwo.new(input) }
+
+  describe "#new" do
+    it "instantiates a set for each line" do
+      expect(ad.input.count).to eq(3)
+      expect(ad.input.class).to eq(Set)
+    end
+
+    it "each input entry contains a valid char index" do
+      expect(ad.input.first[0]).to eq(1)
+    end
+
+    it "each input entry contains an invalid char index" do
+      expect(ad.input.first[1]).to eq(3)
+    end
+
+    it "each input entry contains a char" do
+      expect(ad.input.first[2]).to eq("a")
+    end
+    it "each input entry contains a password" do
+      expect(ad.input.first[3]).to eq("abcde")
+    end
+  end
+
+  describe "#valid_password?" do
+    it "returns false if the password doesn't contain the char at the right point" do
+      expect(ad.valid_password?(1, 3, "a", "bbb")).to be_falsey
+    end
+
+    it "returns false if the password does contain the char at the invalid point" do
+      expect(ad.valid_password?(1, 3, "a", "aaaa")).to be_falsey
+    end
+
+    it "returns true if the password matches the constraints" do
+      expect(ad.valid_password?(1, 3, "a", "aab")).to be_truthy
+    end
+  end
+
+  describe "#valid_password_count" do
+    it "returns a count of valid passwords" do
+      expect(ad.valid_password_count).to eq(1)
     end
   end
 end
