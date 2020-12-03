c2a5a94650babbcc6156ba345a7b8158d9c8d862
Philip Corliss <pcorliss@bigdevbox.com>
2020-12-03 17:29:54 +0000

    sum up all the slopes

--- 03/run.rb
+++ 03/run.rb
@@ -2,7 +2,24 @@
 
 require_relative 'traj'
 
-ad = Advent::Three.new(File.read('./input.txt'))
+input = File.read('./input.txt')
+
+ad = Advent::Three.new(input)
 ad.go_to_bottom!
 puts "Position: #{ad.pos}"
 puts "Trees: #{ad.trees}"
+
+mult = [
+  [1, 1],
+  [3, 1],
+  [5, 1],
+  [7, 1],
+  [1, 2],
+].inject(1) do |acc, slope|
+  ad = Advent::Three.new(input, *slope)
+  ad.go_to_bottom!
+  puts "Trees #{ad.trees} for slope #{slope}"
+  acc *= ad.trees
+end
+
+puts "Mult: #{mult}"
