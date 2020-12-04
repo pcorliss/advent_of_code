e73053bab82b607a15a0aad3ae73cbeddead8b4c
Philip Corliss <pcorliss@bigdevbox.com>
2020-12-04 17:35:39 +0000

    Extra validation

--- 04/spec/passport_spec.rb
+++ 04/spec/passport_spec.rb
@@ -21,6 +21,41 @@ describe Advent do
     EOS
   }
 
+  let(:invalid_passports) {
+    <<~EOS
+      eyr:1972 cid:100
+      hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+
+      iyr:2019
+      hcl:#602927 eyr:1967 hgt:170cm
+      ecl:grn pid:012533040 byr:1946
+
+      hcl:dab227 iyr:2012
+      ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+
+      hgt:59cm ecl:zzz
+      eyr:2038 hcl:74454a iyr:2023
+      pid:3556412378 byr:2007
+    EOS
+  }
+
+  let(:valid_passports) {
+    <<~EOS
+      pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+      hcl:#623a2f
+
+      eyr:2029 ecl:blu cid:129 byr:1989
+      iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+
+      hcl:#888785
+      hgt:164cm byr:2001 iyr:2015 cid:88
+      pid:545766238 ecl:hzl
+      eyr:2022
+
+      iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719
+    EOS
+  }
+
   describe Advent::Four do
     let(:ad) { Advent::Four.new(input) }
 
@@ -33,6 +68,16 @@ describe Advent do
     context "validation" do
       it "reports 2 valid passports" do
         expect(ad.passports.count {|p| p.valid? }).to eq(2)
+      end
+
+      it "handles invalid passports" do
+        ad = Advent::Four.new(invalid_passports)
+        expect(ad.passports.count {|p| p.valid? }).to eq(0)
+      end
+
+      it "handles valid passports" do
+        ad = Advent::Four.new(valid_passports)
+        expect(ad.passports.count {|p| p.valid? }).to eq(4)
       end
     end
   end
