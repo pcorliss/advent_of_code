21970d6d1644bac0a1abab8c1bb6a3b0c2e781c9
Philip Corliss <pcorliss@bigdevbox.com>
2020-12-06 16:39:33 +0000

    blank template

---
+++ blank/spec/blank_spec.rb
@@ -0,0 +1,21 @@
+require '../blank.rb'
+require 'rspec'
+require 'pry'
+
+describe Advent do
+
+  let(:input) {
+    <<~EOS
+    EOS
+  }
+
+  describe Advent::Blank do
+    let(:ad) { Advent::Blank.new(input) }
+
+    describe "#new" do
+    end
+
+    context "validation" do
+    end
+  end
+end
