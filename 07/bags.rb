a8d8589854100aea74dbc7d0f46e98af179523a7
Philip Corliss <pcorliss@bigdevbox.com>
2020-12-07 16:42:08 +0000

    parsing working, but need to refactor

--- 07/bags.rb
+++ 07/bags.rb
@@ -3,7 +3,30 @@ require 'set'
 module Advent
 
   class Bag
+    attr_reader :rules
     def initialize(input)
+      @rules = input.lines.map do |line|
+        l = line.chomp
+        BagReq.new(l)
+      end
+    end
+  end
+
+  class BagReq
+    attr_reader :req
+    ID_REGEX = /^(\w+ \w+) bags contain/
+    CONTAIN_REGEX = /\d+ (\w+ \w+) bag[s]{0,1}/
+
+    def initialize(input)
+      parse!(input)
+    end
+
+    def parse!(input)
+      @req = {}
+      if input =~ ID_REGEX
+        key = $1
+        @req[key] = Set.new(input.scan(CONTAIN_REGEX).flatten)
+      end
     end
   end
 end
--- 07/spec/bags_spec.rb
+++ 07/spec/bags_spec.rb
@@ -6,6 +6,15 @@ describe Advent do
 
   let(:input) {
     <<~EOS
+      light red bags contain 1 bright white bag, 2 muted yellow bags.
+      dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+      bright white bags contain 1 shiny gold bag.
+      muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+      shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+      dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+      vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+      faded blue bags contain no other bags.
+      dotted black bags contain no other bags.
     EOS
   }
 
@@ -13,9 +22,28 @@ describe Advent do
     let(:ad) { Advent::Bag.new(input) }
 
     describe "#new" do
+      it "loads a rules set" do
+        expect(ad.rules.count).to eq(9)
+      end
+
+      it "creates rule objects" do
+        expect(ad.rules.first).to be_a(Advent::BagReq)
+      end
     end
 
     context "validation" do
+    end
+  end
+
+  describe Advent::BagReq do
+    let(:req) { Advent::BagReq.new(input.lines.first) }
+
+    describe "#new" do
+      it "parses the requirement" do
+        expect(req.req).to eq({
+          'light red' => Set.new(['bright white', 'muted yellow'])
+        })
+      end
     end
   end
 end
