b3ce9a4693016756b35e9aa77500b7e33018cd34
Philip Corliss <pcorliss@bigdevbox.com>
2020-12-13 17:58:58 +0000

    Part two solution that works quickly enough to return an answer

--- 13/bus.rb
+++ 13/bus.rb
@@ -3,7 +3,7 @@ require 'set'
 module Advent
 
   class Bus
-    attr_reader :bus, :start
+    attr_reader :bus, :start, :bus_with_offset
 
     def initialize(input)
       @start = input.lines.first.chomp.to_i
@@ -37,28 +37,61 @@ module Advent
     end
 
     # Use a smaller set with just busses and offsets, 5x speedup
-    def contest_match?(t)
-      @bus_with_offset.all? do |b, idx|
-        if (t + idx) % b == 0
-          true
-        else
-          false
-        end
+    def contest_match?(t, bus_offsets = @bus_with_offset)
+      # puts "Checking #{t} #{bus_offsets}"
+      bus_offsets.all? do |b, idx|
+        # puts "#{t} #{idx} #{b} (#{t + idx} % #{b} == 0)"
+        (t + idx) % b == 0
       end
     end
 
     # Use the largest number instead of the first number. Should speed up by a factor of 55 based on input
     # Chinese Remainder Theorem is what folks online are saying may be necessary.
     # Should be able to find solutons using two largest numbers, which will then repeat.
-    def contest
+    # Far too slow to solve the full input. Use contest_prime instead
+    def x_contest
       mult = @bus.max
-      offset = @bus_with_nil.index(mult)
+      offset = @bus_with_offset[mult]
       i = 0
       while true do
         t = mult * i - offset
         return t if contest_match?(t)
         i += 1
         puts "T: #{t} I: #{i}" if i % 1_000_000 == 0
+      end
+    end
+
+    def contest
+      contest_prime(@bus_with_offset)
+    end
+
+    # The following hint ended up being key
+    # The solution to a subset of the problem can be the starting point
+    # The LCM (a * b * ...) can be the multiple we check with
+    # The base case is just a single number
+    # The solution for the full input is less than 2000 checks but more than 200 and returns quickly
+    # http://mathforum.org/library/drmath/view/75030.html
+    def contest_prime(bus_offsets)
+      # puts "Off: #{bus_offsets}"
+      return bus_offsets.first[0] - bus_offsets.first[1] if bus_offsets.length <= 1
+
+      sorted_keys = bus_offsets.keys.sort
+      # puts "Keys: #{sorted_keys}"
+      sorted_keys.shift
+      # puts "shift: #{sorted_keys}"
+
+      start = contest_prime(bus_offsets.slice(*sorted_keys))
+      # puts "Start: #{start}"
+      lcm = sorted_keys.inject(:*)
+      # puts "LCM: #{lcm}"
+
+      i = 0
+      t = start
+      while true do
+        return t if contest_match?(t, bus_offsets)
+        t += lcm
+        i += 1
+        raise "Too many interations" if i > 2000
       end
     end
   end
--- 13/spec/bus_spec.rb
+++ 13/spec/bus_spec.rb
@@ -30,6 +30,43 @@ describe Advent do
       end
     end
 
+    describe "#contest_match?" do
+      it "Matches on multiples" do
+        ad = Advent::Bus.new("0\n7,13,x,x,59,x,31,19")
+        expect(ad.contest_match?(1068781)).to be_truthy
+        # ad = Advent::Bus.new("0\n7,13,x,x,59,x,31,x")
+        # expect(ad.contest_match?(1068781)).to be_truthy
+        expect(1068781 % 7).to eq(0)
+        expect(1068781 % 13).to eq(13 - 1)
+        expect(1068781 % 59).to eq(59 - 4)
+        expect(1068781 % 31).to eq(31 - 6)
+        expect(1068781 % 19).to eq(19 - 7)
+
+        ad = Advent::Bus.new("0\nx,7,3,x,x,x,11")
+        # Adding LCM (7*3*11) will always be truthy
+        expect(ad.contest_match?(181)).to be_truthy
+        expect(ad.contest_match?(412)).to be_truthy
+        expect(ad.contest_match?(643)).to be_truthy
+        expect(ad.contest).to eq(181)
+      end
+    end
+
+    describe "#contest_prime" do
+      it "solves for single elements" do
+        # ad = Advent::Bus.new("0\nx,7,3,x,x,x,11")
+        expect(ad.contest_prime({7 => 1})).to eq(6)
+      end
+
+      it "solves for two elements" do
+        expect(ad.contest_prime({11 => 6, 7 => 1})).to eq(27)
+      end
+
+      it "solves for three elements" do
+        # ad = Advent::Bus.new("0\nx,7,3,x,x,x,11")
+        expect(ad.contest_prime({11 => 6, 7 => 1, 3 => 2})).to eq(181)
+      end
+    end
+
     context "validation" do
       {
         "17,x,13,19" => 3417,
@@ -38,6 +75,7 @@ describe Advent do
         "7,13,x,x,59,x,31,19" => 1068781,
         "67,7,x,59,61" => 1261476,
         "1789,37,47,1889" => 1_202_161_486,
+        "x,7,3,x,x,x,11" => 181,
       }.each do |inp, expected|
         it "validates that #{inp} returns #{expected}" do
           ad = Advent::Bus.new("0\n#{inp}")
