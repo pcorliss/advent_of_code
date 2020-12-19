9377d7506c0b5865370dee562eab80ff2b9d0382
Philip Corliss <pcorliss@bigdevbox.com>
2020-12-19 17:00:35 +0000

    failing tests mid-part-1

--- 19/rules.rb
+++ 19/rules.rb
@@ -3,7 +3,49 @@ require 'set'
 module Advent
 
   class Rules
+    attr_reader :inputs, :rules
     def initialize(input)
+      @rules = []
+      @inputs = []
+      input.each_line do |l|
+        l = l.chomp
+        if l =~ /^\d/
+          @rules << l
+        elsif !l.empty?
+          @inputs << l
+        end
+      end
+    end
+
+    def rule_parser
+      return @rule_parser if @rule_parser
+      @rule_parser = []
+      @rules.each do |r|
+        idx, rule = r.split(": ")
+        rule.gsub!('"', '')
+        subrules = rule.split(" ").map do |subrule|
+          if subrule =~ /\d+/
+            subrule.to_i
+          else
+            subrule
+          end
+        end
+        @rule_parser[idx.to_i] = subrules
+      end
+      @rule_parser
+    end
+
+    #recursive function, takes rules and chars, calls down the stack to check rule by rule
+    def match?(rules, chars)
+      rules.each do |r|
+        puts "Checking: #{r} #{rule_parser[r]} against #{chars}"
+        return false unless rule_parser[r] == [chars]
+        # case r
+        # when r.is_a? Integer
+        #   rule_parser[r]
+        # end
+      end
+      true
     end
   end
 end
--- 19/spec/rules_spec.rb
+++ 19/spec/rules_spec.rb
@@ -6,6 +6,18 @@ describe Advent do
 
   let(:input) {
     <<~EOS
+      0: 4 1 5
+      1: 2 3 | 3 2
+      2: 4 4 | 5 5
+      3: 4 5 | 5 4
+      4: "a"
+      5: "b"
+
+      ababbb
+      bababa
+      abbbab
+      aaabbb
+      aaaabbb
     EOS
   }
 
@@ -13,7 +25,49 @@ describe Advent do
     let(:ad) { Advent::Rules.new(input) }
 
     describe "#new" do
+      it "creates a collection of rules" do
+        expect(ad.rules.count).to eq(6)
+      end
+
+      it "creates a collection of inputs" do
+        expect(ad.inputs.count).to eq(5)
+      end
     end
+
+    describe "rule_parser" do
+      it "breaks rules up into an array" do
+        expect(ad.rule_parser).to be_a(Array)
+        expect(ad.rule_parser.count).to eq(6)
+      end
+
+      it "indexes rules" do
+        expect(ad.rule_parser[5]).to eq(["b"])
+      end
+
+      it "returns a list of sub rules" do
+        expect(ad.rule_parser[0]).to eq([4,1,5])
+      end
+
+      it "handles conditional" do
+        expect(ad.rule_parser[1]).to eq([2,3,'|',3,2])
+      end
+    end
+
+    describe "#match?" do
+      it "matches a single char" do
+        expect(ad.match?([4], "a")).to be_truthy
+        expect(ad.match?([5], "a")).to be_falsey
+      end
+
+      it "matches multiple chars" do
+        expect(ad.match?([4,5], "ab")).to be_truthy
+        expect(ad.match?([4,5], "ba")).to be_falsey
+      end
+      it "matches conditionals"
+      it "matches nested conditionals"
+      it "matches the whole string"
+    end
+
 
     context "validation" do
     end
