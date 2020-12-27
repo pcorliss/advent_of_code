c92f371675e5322f7f3a97c7a1a9a44cd04287ba
Philip Corliss <pcorliss@bigdevbox.com>
2020-12-27 05:34:58 +0000

    Part two completed

--- 2019/02/run.rb
+++ 2019/02/run.rb
@@ -11,3 +11,18 @@ ad.instructions[2] = 2
 puts "Mod   Instructions: #{ad.instructions}"
 ad.run!
 puts "Post  Instructions: #{ad.instructions}"
+
+
+(0..99).each do |noun|
+  (0..99).each do |verb|
+    ad = Advent::Alarm.new(input)
+    ad.instructions[1] = noun
+    ad.instructions[2] = verb
+    ad.run!
+    if ad.instructions[0] == 19690720
+      puts "Noun: #{noun} Verb: #{verb}"
+      puts "Total: #{100*noun + verb}"
+      exit
+    end
+  end
+end
