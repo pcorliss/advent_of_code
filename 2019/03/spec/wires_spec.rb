0135ea01352d02844fdaa573b38b38a2f40793cc
Philip Corliss <pcorliss@bigdevbox.com>
2020-12-27 17:14:56 +0000

    Teasing out grid requirements

--- 2019/03/spec/wires_spec.rb
+++ 2019/03/spec/wires_spec.rb
@@ -3,7 +3,6 @@ require 'rspec'
 require 'pry'
 
 describe Advent do
-
   let(:input) {
     <<~EOS
     EOS
@@ -17,5 +16,33 @@ describe Advent do
 
     context "validation" do
     end
+  end
+end
+
+describe Grid do
+  let (:grid) { Grid.new }
+
+  describe "#new" do
+    it "instantiates an empty infinite grid" do
+      expect(grid).to be_a(Grid)
+      expect(grid.cells).to be_empty
+    end
+  end
+  describe "#draw!" do
+    it "takes a direction, distance and value and marks cells along the route" do
+      grid.draw!([0,1], 4, 1)
+      expect(grid.pos).to eq([0,4])
+      expect(grid.cells.count).to eq(4)
+      expect(grid.cells.values.uniq).to eq([1])
+      expect(grid.cells.keys).to contain_exactly([0,1],[0,2],[0,3],[0,4])
+    end
+
+    it "takes an operator for manipulating values" do
+      grid.draw!([0,1], 4, 1, :+)
+      grid.draw!([0,-1], 4, 2, :+)
+      # Doesn't set origin point so different values will appear
+      expect(grid.cells.values).to include(3, 1, 2)
+    end
+
   end
 end
--- 2019/03/wires.rb
+++ 2019/03/wires.rb
@@ -7,3 +7,33 @@ module Advent
     end
   end
 end
+
+class Grid
+  attr_reader :cells, :pos
+
+  X = 0
+  Y = 1
+
+  def initialize
+    @cells = {}
+    @pos = [0,0]
+  end
+
+  def draw!(direction, distance, val = true, operator = nil)
+    x, y = direction
+    distance.times do |i|
+      new_pos = [@pos[X] + x, @pos[Y] + y]
+      if @cells[new_pos].nil?
+        @cells[new_pos] = val
+      else
+        if operator
+          @cells[new_pos] = @cells[new_pos].__send__(operator, val)
+        else
+          @cells[new_pos] = val
+        end
+      end
+      @pos = new_pos
+    end
+
+  end
+end
