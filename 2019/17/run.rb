1deeeebe72dd1bee010aa94472f48689800869b1
Philip Corliss <pcorliss@bigdevbox.com>
2021-01-04 18:09:26 +0000

    Day 17 init

---
+++ 2019/17/run.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+
+require_relative 'flare'
+
+input = File.read('./input.txt')
+
+ad = Advent::Flare.new(input)
---
+++ 2019/17/spec/flare_spec.rb
@@ -0,0 +1,21 @@
+require './flare.rb'
+require 'rspec'
+require 'pry'
+
+describe Advent do
+
+  let(:input) {
+    <<~EOS
+    EOS
+  }
+
+  describe Advent::Flare do
+    let(:ad) { Advent::Flare.new(input) }
+
+    describe "#new" do
+    end
+
+    context "validation" do
+    end
+  end
+end
