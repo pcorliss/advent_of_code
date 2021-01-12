9a7f20789d2f574ff2ac4d21bbddfa3d8cbfbe19
Philip Corliss <pcorliss@bigdevbox.com>
2021-01-12 18:28:29 +0000

    part 1 completed

--- 2019/23/packets.rb
+++ 2019/23/packets.rb
@@ -6,9 +6,42 @@ module Advent
 
   class Packets
     attr_accessor :debug
+    attr_reader :computers
 
     def initialize(input)
       @debug = false
+      @computers = 50.times.map do |i|
+        a = Advent::IntCode.new(input)
+        a.program_input = i
+        a
+      end
+      @packets = 50.times.map do |i|
+        [i, -1]
+      end
+    end
+
+    def run!
+      addr, x, y = @packets.shift
+      comp = @computers[addr]
+      comp.program_input = x
+      comp.program_input = y if y
+      comp.run!
+      while comp.outputs && !comp.outputs.empty? do
+        @packets << 3.times.map { comp.output }
+        return @packets.last if @packets.last.first == 255
+      end
+      @packets.last
+    end
+
+    def run_until_255
+      last_packet = @packets.last
+      i = 0
+      until last_packet.first == 255 || i > 1000 do
+        last_packet = run!
+        i += 1
+      end
+      puts "Last Packet: #{last_packet}"
+      puts "Iterations: #{i}"
     end
 
     def debug!
--- 2019/23/spec/packets_spec.rb
+++ 2019/23/spec/packets_spec.rb
@@ -4,18 +4,23 @@ require 'pry'
 
 describe Advent do
 
-  let(:input) {
-    <<~EOS
-    EOS
-  }
+  let(:input) { File.read('./spec_input.txt') }
 
   describe Advent::Packets do
     let(:ad) { Advent::Packets.new(input) }
 
     describe "#new" do
+      it "instantiates 50 computers, each with it's own addr (0-49)" do
+        expect(ad.computers.count).to eq(50)
+        expect(ad.computers[49]).to be_a(Advent::IntCode)
+        expect(ad.computers[49].inputs).to eq([49])
+      end
     end
 
     context "validation" do
+      it "retrieves a value form run" do
+        ad.run_until_255
+      end
     end
   end
 end
