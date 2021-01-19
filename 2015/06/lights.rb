2b6ef3502ecd6d759eebe95dd784c8b22df5e34f
Philip Corliss <pcorliss@bigdevbox.com>
2021-01-19 20:51:37 +0000

    Day 6 init

---
+++ 2015/06/lights.rb
@@ -0,0 +1,17 @@
+require 'set'
+require '../lib/grid.rb'
+
+module Advent
+
+  class Lights
+    attr_accessor :debug
+
+    def initialize(input)
+      @debug = false
+    end
+
+    def debug!
+      @debug = true
+    end
+  end
+end
