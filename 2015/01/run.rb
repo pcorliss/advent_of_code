4ba616bcad9624db2d2b25040bc128da09a74913
Philip Corliss <pcorliss@bigdevbox.com>
2021-01-19 17:34:41 +0000

    2015 Day 1 Init

---
+++ 2015/01/run.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+
+require_relative 'parans'
+
+input = File.read('./input.txt')
+
+ad = Advent::Parans.new(input)
---
+++ 2015/01/spec/parans_spec.rb
@@ -0,0 +1,21 @@
+require './parans.rb'
+require 'rspec'
+require 'pry'
+
+describe Advent do
+
+  let(:input) {
+    <<~EOS
+    EOS
+  }
+
+  describe Advent::Parans do
+    let(:ad) { Advent::Parans.new(input) }
+
+    describe "#new" do
+    end
+
+    context "validation" do
+    end
+  end
+end
