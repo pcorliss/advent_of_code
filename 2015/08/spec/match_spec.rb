98e7e8dc08469c64900255ef7a7ed6fd4fd51c88
Philip Corliss <pcorliss@bigdevbox.com>
2021-01-20 21:22:28 +0000

    Day 8 init

---
+++ 2015/08/spec/match_spec.rb
@@ -0,0 +1,21 @@
+require './match.rb'
+require 'rspec'
+require 'pry'
+
+describe Advent do
+
+  let(:input) {
+    <<~EOS
+    EOS
+  }
+
+  describe Advent::Match do
+    let(:ad) { Advent::Match.new(input) }
+
+    describe "#new" do
+    end
+
+    context "validation" do
+    end
+  end
+end
