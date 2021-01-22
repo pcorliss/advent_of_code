cb24ca136c1249bfd4e5ce6765ef06cc25f9e36c
Philip Corliss <pcorliss@bigdevbox.com>
2021-01-22 20:52:42 +0000

    Making it non-specific for my input

--- 2015/11/run.rb
+++ 2015/11/run.rb
@@ -5,7 +5,7 @@ require_relative 'policy'
 input = File.read('./input.txt')
 
 ad = Advent::Policy.new(input)
-next_pw = ad.next_valid_password('hxbxwxba')
+next_pw = ad.next_valid_password(ad.password)
 puts "Next: #{next_pw}"
-next_pw = ad.next_valid_password('hxbxxyzz')
+next_pw = ad.next_valid_password(next_pw)
 puts "Next: #{next_pw}"
