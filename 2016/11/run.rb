0877ddb736f75e1691053a1f6a539bffc8e9d3ae
Philip Corliss <pcorliss@bigdevbox.com>
2021-02-10 01:54:14 +0000

    Day 11 init

---
+++ 2016/11/run.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+
+require_relative 'rad'
+
+input = File.read('./input.txt')
+
+ad = Advent::Rad.new(input)
---
+++ 2016/11/spec/rad_spec.rb
@@ -0,0 +1,21 @@
+require './rad.rb'
+require 'rspec'
+require 'pry'
+
+describe Advent do
+
+  let(:input) {
+    <<~EOS
+    EOS
+  }
+
+  describe Advent::Rad do
+    let(:ad) { Advent::Rad.new(input) }
+
+    describe "#new" do
+    end
+
+    context "validation" do
+    end
+  end
+end
