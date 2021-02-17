ce9ae88424de4aa3f51de7a3cc266d1f1551de4f
Philip Corliss <pcorliss@bigdevbox.com>
2021-02-17 15:37:37 +0000

    Day 18 init

---
+++ 2016/18/run.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+
+require_relative 'tiles'
+
+input = File.read('./input.txt')
+
+ad = Advent::Tiles.new(input)
---
+++ 2016/18/spec/tiles_spec.rb
@@ -0,0 +1,21 @@
+require './tiles.rb'
+require 'rspec'
+require 'pry'
+
+describe Advent do
+
+  let(:input) {
+    <<~EOS
+    EOS
+  }
+
+  describe Advent::Tiles do
+    let(:ad) { Advent::Tiles.new(input) }
+
+    describe "#new" do
+    end
+
+    context "validation" do
+    end
+  end
+end
