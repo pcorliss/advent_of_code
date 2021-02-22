043a631373ad862a65c68887d867ddbff25f26f3
Philip Corliss <pcorliss@bigdevbox.com>
2021-02-22 17:22:09 +0000

    Possibly more efficient

--- 2016/22/gridcomp.rb
+++ 2016/22/gridcomp.rb
@@ -20,6 +20,7 @@ module Advent
           @nodes << [[$1.to_i, $2.to_i], $3.to_i, $4.to_i]
         end
       end
+      @adjacent = {}
     end
 
     def debug!
@@ -47,12 +48,14 @@ module Advent
     end
 
     def adjacent?(a, b)
+      # return @adjacent[[a, b]] if @adjacent.has_key?([a, b])
       xa, ya = a[POS]
       xb, yb = b[POS]
 
       x_delta = (xa - xb).abs
       y_delta = (ya - yb).abs
 
+      #@adjacent[[a,b]] = x_delta + y_delta == 1
       x_delta + y_delta == 1
     end
 
@@ -63,18 +66,30 @@ module Advent
 
       steps = 1
       paths = [[DeepClone.clone(@nodes), DeepClone.clone(data_pos)]]
+      
+      node_lookup = {}
+      @nodes.each_with_index do |n, idx|
+        node_lookup[n[POS]] = idx
+      end
+
+      adjacent_list = Set.new
+      nodes.combination(2).each do |combo|
+        a, b = combo
+        if adjacent?(*combo)
+          adjacent_list.add([a[POS],b[POS]])
+          adjacent_list.add([b[POS],a[POS]])
+        end
+      end
+
       loop do
         new_paths = []
         puts "#{steps}: #{paths.count} #{paths.map(&:last).uniq}" if @debug
         paths.each do |path|
           nodes, path_data_pos = path
           moves = []
-          nodes.combination(2).each do |combo|
-            # TODO: could speed this up by turning nodes into a hash keyed by position and pre-computing adjacent node pairs
-            # binding.pry if @debug && combo.map(&:first).sort == [[1,0],[2,0]]
-            next unless adjacent?(*combo)
+          adjacent_list.each do |a, b|
+            combo = [nodes[node_lookup[a]], nodes[node_lookup[b]]]
             moves << combo if viable_pair?(*combo)
-            moves << combo.reverse if viable_pair?(*combo.reverse)
           end
 
           puts "\t #{steps} Working on: #{moves.count} moves" if @debug && false
@@ -86,7 +101,13 @@ module Advent
             a = new_nodes.find {|n| n == a}
             b = new_nodes.find {|n| n == b}
             new_data_pos = path_data_pos
+          begin
             new_data_pos = b[POS] if a[POS] == path_data_pos
+          rescue => e
+            binding.pry
+            raise e
+          end
+
 
             b[USED] += a[USED]
             a[USED] = 0
@@ -109,11 +130,6 @@ module Advent
         raise "Too many iterations!!! #{steps}"  if steps >= 10
         steps += 1
       end
-      # @nodes.combination(2).each do |combo|
-      #
-      # end
-
-      steps
     end
   end
 end
