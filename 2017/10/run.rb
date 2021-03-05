3c5473a0c8f0d40bfaba0da3bb02f1edee3c870f
Philip Corliss <pcorliss@bigdevbox.com>
2021-03-05 16:09:56 +0000

    Day 10 init

---
+++ 2017/10/run.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+
+require_relative 'knots'
+
+input = File.read('./input.txt')
+
+ad = Advent::Knots.new(input)
---
+++ 2017/10/spec/knots_spec.rb
@@ -0,0 +1,21 @@
+require './knots.rb'
+require 'rspec'
+require 'pry'
+
+describe Advent do
+
+  let(:input) {
+    <<~EOS
+    EOS
+  }
+
+  describe Advent::Knots do
+    let(:ad) { Advent::Knots.new(input) }
+
+    describe "#new" do
+    end
+
+    context "validation" do
+    end
+  end
+end
