fa2ebea653b8d9663c1e0681a63ed5d697cd99d6
Philip Corliss <pcorliss@bigdevbox.com>
2021-03-09 17:04:18 +0000

    Day 14 init

---
+++ 2017/14/spec/defrag_spec.rb
@@ -0,0 +1,21 @@
+require './defrag.rb'
+require 'rspec'
+require 'pry'
+
+describe Advent do
+
+  let(:input) {
+    <<~EOS
+    EOS
+  }
+
+  describe Advent::Defrag do
+    let(:ad) { Advent::Defrag.new(input) }
+
+    describe "#new" do
+    end
+
+    context "validation" do
+    end
+  end
+end
