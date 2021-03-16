f2ec5e73b04640f87faa5a5dc8f6e728ec940f7e
Philip Corliss <pcorliss@bigdevbox.com>
2021-03-16 20:46:09 +0000

    Day 24 init

---
+++ 2017/24/run.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+
+require_relative 'bridge'
+
+input = File.read('./input.txt')
+
+ad = Advent::Bridge.new(input)
---
+++ 2017/24/spec/bridge_spec.rb
@@ -0,0 +1,21 @@
+require './bridge.rb'
+require 'rspec'
+require 'pry'
+
+describe Advent do
+
+  let(:input) {
+    <<~EOS
+    EOS
+  }
+
+  describe Advent::Bridge do
+    let(:ad) { Advent::Bridge.new(input) }
+
+    describe "#new" do
+    end
+
+    context "validation" do
+    end
+  end
+end
