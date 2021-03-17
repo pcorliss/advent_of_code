27477163e5dd479d687c71132f9b73edb6552c84
Philip Corliss <pcorliss@bigdevbox.com>
2021-03-17 20:37:09 +0000

    Day 1 Init

---
+++ 2018/01/src/main.ts
@@ -0,0 +1,9 @@
+let message = 'Hello World';
+console.log(message);
+message = 'bar';
+console.log(6 + 6);
+const cake = 'Cake';
+console.log(cake, message);
+console.log(
+  'This is a very long line 0123456789 0123456789 0123456789 0123456789 0123456789 0123456789 0123456789 0123456789 0123456789 0123456789 0123456789 ',
+);
---
+++ 2018/01/tests/HelloWorld.spec.ts
@@ -0,0 +1,10 @@
+//import { HelloWorld } from '../src/HelloWorld';
+import { expect } from 'chai';
+import 'mocha';
+
+describe('Truthiness',
+  () => {
+    it('should return true', () => {
+      expect(true).to.equal(true);
+  });
+});
