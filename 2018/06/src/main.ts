5657a068066223398e9c4018787def3cdc300b3a
Philip Corliss <pcorliss@bigdevbox.com>
2021-03-24 15:58:25 +0000

    Day 6 init

---
+++ 2018/06/src/main.ts
@@ -0,0 +1,5 @@
+export default class Advent {
+
+  constructor(input: string) {
+  }
+}
---
+++ 2018/06/tests/main.spec.ts
@@ -0,0 +1,19 @@
+import Advent from '../src/main';
+import { expect } from 'chai';
+
+describe('Advent', () => {
+  const input: string = `
+`.trim();
+
+  let ad;
+
+  beforeEach(() => {
+    ad = new Advent(input);
+  });
+
+  describe('#new', () => {
+    // it('inits a polymer chain', () => {
+    //   expect(ad.polymer).to.eql('dabAcCaCBAcCcaDA');
+    // });
+  });
+});
