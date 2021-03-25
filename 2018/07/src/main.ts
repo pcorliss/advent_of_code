c5f86efd095a64ac80a4875a1f63de93e6c91186
Philip Corliss <pcorliss@bigdevbox.com>
2021-03-25 17:51:48 +0000

    Day 7 Init

---
+++ 2018/07/src/main.ts
@@ -0,0 +1,3 @@
+export default class Advent {
+  constructor(input: string) { }
+}
---
+++ 2018/07/tests/main.spec.ts
@@ -0,0 +1,18 @@
+import Advent from '../src/main';
+import { expect } from 'chai';
+
+describe('Advent', () => {
+  const input: string = `
+`.trim();
+
+  let ad;
+
+  beforeEach(() => {
+    ad = new Advent(input);
+  });
+
+  describe('#new', () => {
+    // it('inits a ', () => {
+    // });
+  });
+});
