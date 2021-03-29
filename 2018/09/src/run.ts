43fe49bde5260282185d95925674f119468049f8
Philip Corliss <pcorliss@bigdevbox.com>
2021-03-29 22:05:03 +0000

    Part 2 compelted

--- 2018/09/src/run.ts
+++ 2018/09/src/run.ts
@@ -3,7 +3,12 @@ import fs = require('fs');
 
 const input_str: string = fs.readFileSync('input.txt', 'utf8');
 
-const ad = new Advent(input_str);
+let ad = new Advent(input_str);
 ad.play();
 console.log(`Player Scores: ${ad.playerScores}`);
+console.log(`High Score: ${Math.max(...ad.playerScores)}`);
+
+ad = new Advent(input_str);
+ad.marbles *= 100;
+ad.play();
 console.log(`High Score: ${Math.max(...ad.playerScores)}`);
