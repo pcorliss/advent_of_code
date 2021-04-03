a77740e0ce9f4a7d6b1bee1a7fe3a9be75d4a6ca
Philip Corliss <pcorliss@bigdevbox.com>
2021-04-03 16:55:49 +0000

    Stopping for lunch

--- 2018/15/src/main.ts
+++ 2018/15/src/main.ts
@@ -1,6 +1,71 @@
+type Actor = {
+  x: number;
+  y: number;
+  hp: number;
+  elf: boolean;
+  goblin: boolean;
+  t: string;
+};
+
 class Advent {
+  grid: (boolean | Actor)[][];
+  actors: Actor[];
+
   constructor(input: string) {
+    this.actors = [];
+    this.grid = input.split('\n').map((line, y) => {
+      return line.split('').map((char, x) => {
+        if (char == 'G' || char == 'E') {
+          this.actors.push({
+            x: x,
+            y: y,
+            hp: 200,
+            elf: char == 'E',
+            goblin: char == 'G',
+            t: char,
+          });
+          return this.actors[this.actors.length - 1];
+        }
+        return char != '#';
+      });
+    });
   }
+
+  deltas = [
+    [0, -1],
+    [-1, 0],
+    [1, 0],
+    [0, 1],
+  ];
+
+  move(actor: Actor): void {
+    // identifying all possible targets
+    const enemies = this.actors.filter((a) => a.t != actor.t);
+    // identifies all of the open squares (.) that are in range of each target;
+    const openSquares: [number, number][] = [];
+    for (const e of enemies) {
+      for (const [xD, yD] of this.deltas) {
+        const [x, y] = [xD + e.x, yD + e.y];
+        // return if I am already adjacent to a target
+        if (x == actor.x && y == actor.y) {
+          return;
+        }
+        // If this space is open
+        if (this.grid[y][x] === true) {
+          openSquares.push([x, y]);
+        }
+      }
+    }
+    // return if there are no adjacent squares open to a target
+    if (openSquares.length == 0) {
+      return;
+    }
+    // DFS to find reachable
+    // any ties sort by readable order (y * 1000 + x)
+  }
+
+  attack(actor: Actor): void {}
+  round(): void {}
 }
 
 export { Advent };
--- 2018/15/tests/main.spec.ts
+++ 2018/15/tests/main.spec.ts
@@ -3,6 +3,13 @@ import { expect } from 'chai';
 
 describe('Advent', () => {
   const input: string = `
+#######
+#.G...#
+#...EG#
+#.#.#G#
+#..G#E#
+#.....#
+#######
 `.trim();
 
   let ad;
@@ -12,8 +19,48 @@ describe('Advent', () => {
   });
 
   describe('#new', () => {
-    // it('inits a grid serial number', () => {
-    //   expect(ad.serial).to.eql(8);
-    // });
+    it('inits a battle field as a grid with [y][x] coords', () => {
+      expect(ad.grid[0][0]).to.eql(false);
+      expect(ad.grid[1][1]).to.eql(true);
+      expect(ad.grid[6][6]).to.eql(false);
+    });
+
+    it('inits a battle field with actor references', () => {
+      expect(ad.grid[1][2].t).to.eql('G');
+      expect(ad.grid[2][4].t).to.eql('E');
+    });
+
+    it('inits actors', () => {
+      expect(ad.actors).to.have.lengthOf(6);
+      expect(ad.actors[0]).to.eql({
+        x: 2,
+        y: 1,
+        hp: 200,
+        t: 'G',
+        elf: false,
+        goblin: true,
+      });
+    });
   });
+
+  describe('#move', () => {
+    it('does not move if it is in attack range', () => {
+      const elf = ad.actors[1];
+      expect(elf.elf).to.be.true;
+      ad.move(elf);
+      expect(elf.x).to.eql(4);
+      expect(elf.y).to.eql(2);
+    });
+
+    it('moves one step', () => {
+      const gob = ad.actors[0];
+      expect(gob.goblin).to.be.true;
+      ad.move(gob);
+      expect(gob.x).to.eql(3);
+      expect(gob.y).to.eql(2);
+    });
+  });
+
+  // the order in which units take their turns within a round is the reading order of their starting positions in that round
+  // If no targets remain, combat ends.
 });
