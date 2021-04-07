210680f269f81fbabdd78de49152c39fcc9cbbf4
Philip Corliss <pcorliss@bigdevbox.com>
2021-04-07 17:39:44 +0000

    Day 18 init

---
+++ 2018/18/src/run.ts
@@ -0,0 +1,6 @@
+import { Advent } from '../src/main';
+import fs = require('fs');
+
+const input_str: string = fs.readFileSync('input.txt', 'utf8');
+
+const ad = new Advent(input_str);
---
+++ 2018/18/tests/main.spec.ts
@@ -0,0 +1,19 @@
+import { Advent } from '../src/main';
+import { expect } from 'chai';
+
+describe('Advent', () => {
+  const input: string = `
+`.trim();
+
+  let ad;
+
+  beforeEach(() => {
+    ad = new Advent(input);
+  });
+
+  describe('#new', () => {
+    // it('inits a grid serial number', () => {
+    //   expect(ad.serial).to.eql(8);
+    // });
+  });
+});
