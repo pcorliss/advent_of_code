7a1c241647bd1ec07925016bc8a6ef6089d48d72
Philip Corliss <pcorliss@bigdevbox.com>
2021-04-08 19:42:09 +0000

    Part 2 completed

--- 2018/19/src/main.ts
+++ 2018/19/src/main.ts
@@ -1,5 +1,104 @@
 class Advent {
+  registers: number[];
+  instructionPointerRegister: number;
+  instructions: [string, number, number, number][];
+
   constructor(input: string) {
+    this.registers = [0, 0, 0, 0, 0, 0];
+    this.instructions = [];
+    for (const line of input.split('\n')) {
+      if (this.instructionPointerRegister == null) {
+        const [, ip] = line.split(' ');
+        this.instructionPointerRegister = parseInt(ip);
+      } else {
+        const [op, a, b, c] = line.split(' ');
+        this.instructions.push([op, parseInt(a), parseInt(b), parseInt(c)]);
+      }
+    }
+  }
+
+  process(op: string, a: number, b: number, c: number): number[] {
+    switch (op) {
+      case 'addr':
+        this.registers[c] = this.registers[a] + this.registers[b];
+        break;
+      case 'addi':
+        this.registers[c] = this.registers[a] + b;
+        break;
+      case 'mulr':
+        this.registers[c] = this.registers[a] * this.registers[b];
+        break;
+      case 'muli':
+        this.registers[c] = this.registers[a] * b;
+        break;
+      case 'banr':
+        this.registers[c] = this.registers[a] & this.registers[b];
+        break;
+      case 'bani':
+        this.registers[c] = this.registers[a] & b;
+        break;
+      case 'borr':
+        this.registers[c] = this.registers[a] | this.registers[b];
+        break;
+      case 'bori':
+        this.registers[c] = this.registers[a] | b;
+        break;
+      case 'setr':
+        this.registers[c] = this.registers[a];
+        break;
+      case 'seti':
+        this.registers[c] = a;
+        break;
+      case 'gtir':
+        this.registers[c] = a > this.registers[b] ? 1 : 0;
+        break;
+      case 'gtri':
+        this.registers[c] = this.registers[a] > b ? 1 : 0;
+        break;
+      case 'gtrr':
+        this.registers[c] = this.registers[a] > this.registers[b] ? 1 : 0;
+        break;
+      case 'eqir':
+        this.registers[c] = a == this.registers[b] ? 1 : 0;
+        break;
+      case 'eqri':
+        this.registers[c] = this.registers[a] == b ? 1 : 0;
+        break;
+      case 'eqrr':
+        this.registers[c] = this.registers[a] == this.registers[b] ? 1 : 0;
+        break;
+      default:
+        throw `Unrecognized op code ${op}`;
+    }
+    return this.registers;
+  }
+
+  step(bug = false): void {
+    const instructionPointer = this.registers[this.instructionPointerRegister];
+    const instruction = this.instructions[instructionPointer];
+    const startingRegister = this.registers.slice();
+    this.process(...instruction);
+    if (bug) {
+      console.log(
+        'ip=',
+        instructionPointer,
+        startingRegister,
+        instruction,
+        this.registers,
+      );
+    }
+    this.registers[this.instructionPointerRegister]++;
+  }
+
+  halt(): boolean {
+    const inst = this.registers[this.instructionPointerRegister];
+    return inst < 0 || inst >= this.instructions.length;
+  }
+
+  run(bug = false): void {
+    while (!this.halt()) {
+      this.step(bug);
+    }
   }
 }
 
--- 2018/19/src/run.ts
+++ 2018/19/src/run.ts
@@ -4,3 +4,57 @@ import fs = require('fs');
 const input_str: string = fs.readFileSync('input.txt', 'utf8');
 
 const ad = new Advent(input_str);
+ad.run(false);
+console.log('Registers:', ad.registers);
+
+let r = [0, 0, 0, 0, 0, 0];
+
+if (r[0] == 1) {
+  r[2] = 10550400;
+  r[5] = 10551364;
+  r[0] = 0;
+} else {
+  r[2] = 128;
+  r[5] = 964;
+}
+
+r[3] = 1;
+while (r[3] <= r[5]) {
+  r[1] = 1;
+  // console.log('ip= 2', r);
+  while (r[1] <= r[5]) {
+    r[2] = r[3] * r[1];
+    // console.log('ip= 3', r, r[2] == r[5]);
+    if (r[2] == r[5]) {
+      r[0] += r[3];
+    }
+    r[1]++;
+  }
+  r[3]++;
+}
+
+console.log('R:', r);
+r = [0, 0, 0, 0, 0, 964];
+
+for (let i = 1; i <= r[5]; i++) {
+  if (r[5] % i == 0) {
+    r[0] += i;
+  }
+}
+console.log('R:', r);
+
+r = [0, 0, 0, 0, 0, 10551364];
+
+for (let i = 1; i <= r[5]; i++) {
+  if (r[5] % i == 0) {
+    r[0] += i;
+  }
+}
+console.log('R:', r);
+
+// ad = new Advent(input_str);
+// ad.registers[0] = 1;
+// ad.run(true);
+// console.log('Registers:', ad.registers);
+
+// Too low 10551365
