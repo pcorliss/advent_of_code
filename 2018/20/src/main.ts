030ea2ee856811be078448936a966803d37efd3e
Philip Corliss <pcorliss@bigdevbox.com>
2021-04-09 22:56:34 +0000

    Correct grid rendering

--- 2018/20/src/main.ts
+++ 2018/20/src/main.ts
@@ -1,6 +1,185 @@
-class Advent {
-  constructor(input: string) {
+class Grid<T> {
+  map: Map<string, T>;
+
+  constructor() {
+    this.map = new Map();
   }
+
+  pair(x: number, y: number): string {
+    return [x, y].toString();
+  }
+
+  unpair(z: string): [number, number] {
+    const [a, b] = z.split(',', 2).map((n) => parseInt(n));
+    return [a, b];
+  }
+
+  get(x: number, y: number): T {
+    return this.map.get(this.pair(x, y));
+  }
+
+  set(x: number, y: number, val: T): void {
+    this.map.set(this.pair(x, y), val);
+  }
+
+  keys(): [number, number][] {
+    return [...this.map.keys()].map((k) => this.unpair(k));
+  }
+
+  entries(): [number, number, T][] {
+    return [...this.map.entries()].map(([k, v]) => {
+      const [x, y] = this.unpair(k);
+      return [x, y, v];
+    });
+  }
+
+  minMax(): [[number, number], [number, number]] {
+    const minMax: [[number, number], [number, number]] = [
+      [Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER],
+      [Number.MIN_SAFE_INTEGER, Number.MIN_SAFE_INTEGER],
+    ];
+    for (const [x, y] of this.keys()) {
+      if (x > minMax[1][0]) minMax[1][0] = x;
+      if (x < minMax[0][0]) minMax[0][0] = x;
+      if (y > minMax[1][1]) minMax[1][1] = y;
+      if (y < minMax[0][1]) minMax[0][1] = y;
+    }
+    return minMax;
+  }
+
+  render(): string {
+    if (this.keys().length == 0) return '';
+    const minMax = this.minMax();
+    let renderStr = '#'.repeat((minMax[1][0] - minMax[0][0] + 1) * 2 + 1);
+    renderStr += '\n';
+    for (let y = minMax[0][1]; y <= minMax[1][1]; y++) {
+      let nextLine = '#';
+      renderStr += '#';
+      for (let x = minMax[0][0]; x <= minMax[1][0]; x++) {
+        const cell: number | any = this.get(x, y) || 0;
+        if (x == 0 && y == 0) {
+          renderStr += 'X';
+        } else {
+          renderStr += cell != 0 ? '.' : '#';
+        }
+        renderStr += (cell & 4) == 4 ? '|' : '#';
+        nextLine += (cell & 2) == 2 ? '-' : '#';
+        nextLine += '#';
+      }
+      renderStr += '\n' + nextLine + '\n';
+    }
+    return renderStr.trim();
+  }
+}
+
+class Advent {
+  pathRegex: string;
+  grid: Grid<number>;
+
+  constructor(input: string) {
+    this.pathRegex = input;
+    this.grid = new Grid();
+  }
+
+  directionMap = {
+    W: [-1, 0],
+    E: [1, 0],
+    S: [0, 1],
+    N: [0, -1],
+  };
+
+  directionBitMap = {
+    N: 1,
+    S: 2,
+    E: 4,
+    W: 8,
+  };
+
+  reverseDirectionBitMap = {
+    N: 2,
+    S: 1,
+    E: 8,
+    W: 4,
+  };
+
+  deepCopy(obj: any): any {
+    return JSON.parse(JSON.stringify(obj));
+  }
+
+  fill(path: string): number {
+    const stack: Array<any> = [];
+    let branches: Array<any> = [[0, 0]];
+    let newBranches: Array<any> = [];
+    for (const [idx, char] of path.split('').entries()) {
+      switch (char) {
+        case '^':
+        case '(':
+          newBranches = [];
+          stack.push(this.deepCopy(branches));
+          break;
+        case ')':
+        case '$':
+          stack.pop();
+          newBranches.concat(branches);
+          branches = newBranches;
+          break;
+        case '|':
+          newBranches.concat(branches);
+          branches = this.deepCopy(stack[stack.length - 1]);
+          break;
+        default:
+          for (let i = 0; i < branches.length; i++) {
+            let [x, y] = branches[i];
+            const [xD, yD] = this.directionMap[char];
+            const dirBit = this.directionBitMap[char];
+            const revBit = this.reverseDirectionBitMap[char];
+            this.grid.set(x, y, (this.grid.get(x, y) || 0) + dirBit);
+            x += xD;
+            y += yD;
+            branches[i] = [x, y];
+            this.grid.set(x, y, (this.grid.get(x, y) || 0) + revBit);
+            console.log(char, ...stack);
+            console.log(this.grid.render());
+          }
+      }
+    }
+    return 0;
+  }
+
+  // fill(path: string): number {
+  //   let steps = 0;
+  //   let workingBranch: [string, number, number, number][] = null;
+  //   let working: [string, number, number, number][][] = null;
+  //   const stack: [string, number, number, number][][][] = [];
+  //   let x, y = 0;
+  //   const branches: [number, number][] = [];
+
+  //   for (let i = 0; i < path.length; i++) {
+  //     const char = path[i];
+  //     switch (char) {
+  //       case '^':
+  //       case '(':
+  //         stack.push([[]]);
+  //         working = stack[stack.length - 1];
+  //         workingBranch = working[0];
+  //         break;
+  //       case ')':
+  //         working = stack[stack.length - 2];
+  //         workingBranch = working[working.length - 1];
+  //         const prevWorking = stack.pop();
+  //       case '$':
+  //         break;
+  //       case '|':
+  //         working.push([]);
+  //         workingBranch = working[working.length - 1];
+  //         break;
+  //       default:
+  //         workingBranch.push([char, x, y, steps]);
+  //     }
+  //   }
+
+  //   return steps;
+  // }
 }
 
 export { Advent };
--- 2018/20/tests/main.spec.ts
+++ 2018/20/tests/main.spec.ts
@@ -3,6 +3,7 @@ import { expect } from 'chai';
 
 describe('Advent', () => {
   const input: string = `
+^WSSEESWWWNW(S|NENNEEEENN(ESSSSW(NWSW|SSEN)|WSWWN(E|WWS(E|SS))))$
 `.trim();
 
   let ad;
@@ -12,8 +13,72 @@ describe('Advent', () => {
   });
 
   describe('#new', () => {
-    // it('inits a grid serial number', () => {
-    //   expect(ad.serial).to.eql(8);
-    // });
+    it('inits a regex as a string', () => {
+      expect(ad.pathRegex).to.eql(
+        '^WSSEESWWWNW(S|NENNEEEENN(ESSSSW(NWSW|SSEN)|WSWWN(E|WWS(E|SS))))$',
+      );
+    });
+
+    it('inits an empty grid', () => {
+      expect(ad.grid.render()).to.eql('');
+    });
+  });
+
+  describe('#fill', () => {
+    const expectedRender: string = `
+#########
+#.|.|.|.#
+#-#######
+#.|.|.|.#
+#-#####-#
+#.#.#X|.#
+#-#-#####
+#.|.|.|.#
+#########
+    `.trim();
+
+    it('fills out the grid', () => {
+      ad.fill('^ENWWW(NEEE|SSE(EE|N))$');
+      expect(ad.grid.render()).to.eql(expectedRender);
+    });
+
+    const expectedRender2: string = `
+###############
+#.|.|.|.#.|.|.#
+#-###-###-#-#-#
+#.|.#.|.|.#.#.#
+#-#########-#-#
+#.#.|.|.|.|.#.#
+#-#-#########-#
+#.#.#.|X#.|.#.#
+###-#-###-#-#-#
+#.|.#.#.|.#.|.#
+#-###-#####-###
+#.|.#.|.|.#.#.#
+#-#-#####-#-#-#
+#.#.|.|.|.#.|.#
+###############
+        `.trim();
+
+    it('fills out the grid', () => {
+      ad.fill(ad.pathRegex);
+      expect(ad.grid.render()).to.eql(expectedRender2);
+    });
+
+    const directionStep = {
+      // '^WNE$': 3,
+      // '^(EN|WS)$': 2,
+      // '^N(E|W)N$': 3,
+      // '^ENWWW(NEEE|SSE(EE|N))$': 10,
+      // '^ENNWSWW(NEWS|)SSSEEN(WNSE|)EE(SWEN|)NNN$': 18,
+      // '^ESSWWN(E|NNENN(EESS(WNSE|)SSS|WWWSSSSE(SW|NNNE)))$': 23,
+      // '^WSSEESWWWNW(S|NENNEEEENN(ESSSSW(NWSW|SSEN)|WSWWN(E|WWS(E|SS))))$': 31,
+    };
+    for (const [inp, steps] of Object.entries(directionStep)) {
+      it(`returns the max steps ${steps} required for ${inp}`, () => {
+        console.log(ad.grid.render());
+        expect(ad.fill(inp)).to.eql(steps);
+      });
+    }
   });
 });
