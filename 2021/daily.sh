6e7163db843d5084031ec4e6437fecfe6c5f997c
Philip Corliss <pcorliss@50projects.com>
2021-11-30 21:01:00 -0600

    Use gsed instead of mac sed

--- 2021/daily.sh
+++ 2021/daily.sh
@@ -7,10 +7,11 @@ set -o verbose
 
 DATE=$(date "+%d")
 DAY=${1:-$DATE}
+echo "Day: ${DAY}"
 
 cp -pr template ${DAY}
 pushd ${DAY}
-find . | grep .go | xargs sed -i "s/template/${DAY}/g"
+find . | grep .go | xargs gsed -i "s/template/${DAY}/g"
 mv template.go day${DAY}.go
 mv template_test.go day${DAY}_test.go
 popd
