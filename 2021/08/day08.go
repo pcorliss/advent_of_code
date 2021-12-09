aa593ed9821df3ffd6e124dc220b33900e46d5bf
Philip Corliss <pcorliss@50projects.com>
2021-12-08 21:02:53 -0600

    Day 8 Part 2 complete

--- 2021/08/day08.go
+++ 2021/08/day08.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"sort"
 	"strings"
 )
 
@@ -16,6 +17,7 @@ func main() {
 	fmt.Println("Day 08")
 	fmt.Println("Part 01")
 	fmt.Println("Result: ", Part1(string(inputStr)))
+	// Info(string(inputStr))
 
 	fmt.Println("Part 02")
 	fmt.Println("Result: ", Part2(string(inputStr)))
@@ -53,6 +55,139 @@ func Part1(input string) int {
 	return count
 }
 
+func Info(input string) {
+	signals := StringToSignals(input)
+	match := [8]bool{}
+	for _, s := range signals {
+		for _, out := range s.sigs {
+			l := len(out)
+			switch l {
+			case 2, 3, 4, 7:
+				match[l] = true
+			}
+		}
+		for _, out := range s.outs {
+			l := len(out)
+			switch l {
+			case 2, 3, 4, 7:
+				match[l] = true
+			}
+		}
+		fmt.Print(match[2] && match[3] && match[4] && match[7])
+		fmt.Print(" - ")
+		fmt.Println(s)
+	}
+}
+
+// https://stackoverflow.com/questions/22688651/golang-how-to-sort-string-or-byte
+func SortString(w string) string {
+	s := strings.Split(w, "")
+	sort.Strings(s)
+	return strings.Join(s, "")
+}
+
+func CombineChars(a string, b string) string {
+	m := make(map[string]bool)
+	for _, char := range strings.Split(a, "") {
+		m[char] = true
+	}
+	for _, char := range strings.Split(b, "") {
+		m[char] = true
+	}
+	keys := make([]string, 0, len(m))
+	for k := range m {
+		keys = append(keys, k)
+	}
+	return strings.Join(keys, "")
+}
+
+func matchingChars(a string, b string) int {
+	m := make(map[string]bool)
+	count := 0
+	for _, char := range strings.Split(a, "") {
+		m[char] = true
+	}
+	for _, char := range strings.Split(b, "") {
+		if m[char] {
+			count++
+		}
+	}
+	return count
+}
+
+func Deduce(s signal) map[string]int {
+	out := make(map[string]int)
+	lookup := [10]string{}
+	sigs := []string{}
+	sigs = append(sigs, s.sigs...)
+	sigs = append(sigs, s.outs...)
+
+	// We should skip duplicates
+	for _, s := range sigs {
+		sorted := SortString(s)
+		l := len(s)
+		switch l {
+		case 2:
+			out[sorted] = 1
+			lookup[1] = sorted
+		case 3:
+			out[sorted] = 7
+			lookup[7] = sorted
+		case 4:
+			out[sorted] = 4
+			lookup[4] = sorted
+		case 7:
+			out[sorted] = 8
+			lookup[8] = sorted
+		}
+	}
+
+	// 1,4,7 matching chars * 100, 10, and 1 and summed
+	l5Lookup := make(map[int]int)
+	l5Lookup[122] = 2
+	l5Lookup[233] = 3
+	l5Lookup[132] = 5
+	l6Lookup := make(map[int]int)
+	l6Lookup[222] = 0
+	l6Lookup[132] = 6
+	l6Lookup[243] = 9
+
+	// We should skip lines we already have
+	for _, s := range sigs {
+		sorted := SortString(s)
+		l := len(s)
+		switch l {
+		case 5:
+			sum := 100 * matchingChars(lookup[1], sorted)
+			sum += 10 * matchingChars(lookup[4], sorted)
+			sum += matchingChars(lookup[7], sorted)
+			out[sorted] = l5Lookup[sum]
+			// fmt.Println("Sorted:", sorted, "Sum: ", sum, "Lookup:", l5Lookup[sum])
+		case 6:
+			sum := 100 * matchingChars(lookup[1], sorted)
+			sum += 10 * matchingChars(lookup[4], sorted)
+			sum += matchingChars(lookup[7], sorted)
+			out[sorted] = l6Lookup[sum]
+			// if sorted == "abcdef" {
+			// 	fmt.Println("Sorted:", sorted, "Sum: ", sum, "Lookup:", l6Lookup[sum])
+			// }
+		}
+	}
+
+	return out
+}
+
 func Part2(input string) int {
-	return 0
+	signals := StringToSignals(input)
+	sum := 0
+	for _, s := range signals {
+		mapping := Deduce(s)
+		mult := 10000
+		for _, out := range s.outs {
+			sorted := SortString(out)
+			mult = mult / 10
+			sum += mult * mapping[sorted]
+		}
+	}
+	return sum
 }
--- 2021/08/day08_test.go
+++ 2021/08/day08_test.go
@@ -24,6 +24,31 @@ func TestPart1(t *testing.T) {
 	assert.Equal(t, 26, Part1(inputStr), "they should be equal")
 }
 
+func TestDeduceFindsEasyMaps(t *testing.T) {
+	str := "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf"
+	signal := StringToSignals(str)[0]
+	assert.Equal(t, 1, Deduce(signal)["ab"], "they should be equal")
+	assert.Equal(t, 4, Deduce(signal)["abef"], "they should be equal")
+	assert.Equal(t, 7, Deduce(signal)["abd"], "they should be equal")
+	assert.Equal(t, 8, Deduce(signal)["abcdefg"], "they should be equal")
+}
+
+func TestDeduceFindsHardMapsSizeFive(t *testing.T) {
+	str := "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf"
+	signal := StringToSignals(str)[0]
+	assert.Equal(t, 5, Deduce(signal)["bcdef"], "they should be equal")
+	assert.Equal(t, 2, Deduce(signal)["acdfg"], "they should be equal")
+	assert.Equal(t, 3, Deduce(signal)["abcdf"], "they should be equal")
+}
+
+func TestDeduceFindsHardMapsSizeSix(t *testing.T) {
+	str := "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf"
+	signal := StringToSignals(str)[0]
+	assert.Equal(t, 0, Deduce(signal)["abcdeg"], "they should be equal")
+	assert.Equal(t, 6, Deduce(signal)["bcdefg"], "they should be equal")
+	assert.Equal(t, 9, Deduce(signal)["abcdef"], "they should be equal")
+}
+
 func TestPart2(t *testing.T) {
-	assert.Equal(t, 0, Part2(inputStr), "they should be equal")
+	assert.Equal(t, 61229, Part2(inputStr), "they should be equal")
 }
