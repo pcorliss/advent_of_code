5e125ab4a604c7a78b6e318a6aff251df48fc7c0
Philip Corliss <pcorliss@50projects.com>
2021-12-17 20:16:06 -0600

    Day 17 part 1 complete

--- 2021/17/day17.go
+++ 2021/17/day17.go
@@ -41,11 +41,13 @@ func StringToTarget(input string) Target {
 	for _, part := range parts {
 		pieces := strings.Split(part, "=")
 		tRange := strings.Split(pieces[1], "..")
-		if tRange[0] > tRange[1] {
-			tRange[0], tRange[1] = tRange[1], tRange[0]
-		}
 		a, _ := strconv.Atoi(tRange[0])
 		b, _ := strconv.Atoi(tRange[1])
+		if a > b {
+			fmt.Println(pieces, a, b)
+			panic("Out of Order")
+			// tRange[0], tRange[1] = tRange[1], tRange[0]
+		}
 		if pieces[0] == "x" {
 			target.x_start = a
 			target.x_end = b
@@ -86,7 +88,7 @@ func IntersectsTarget(vel Point, t Target) (bool, int) {
 		// if i > 160 {
 		// 	panic("Too many iterations!!!")
 		// }
-		if p.x > t.x_end || p.y < t.y_end {
+		if p.x > t.x_end || p.y < t.y_start {
 			return false, 0
 		}
 	}
@@ -94,20 +96,35 @@ func IntersectsTarget(vel Point, t Target) (bool, int) {
 
 func MaxVel(t Target) (Point, int) {
 	maxPoint := Point{}
+	minX, maxX := 0, 0
+	minY, maxYY := 0, 0
 	maxY := 0
 	fmt.Println(t.x_end, -t.y_start, t)
-	for x := 0; x < t.x_end; x++ {
-		for y := 0; y < -t.y_start; y++ {
+	for x := 0; x <= t.x_end; x++ {
+		for y := t.y_start; y <= -t.y_start; y++ {
 			boo, max := IntersectsTarget(Point{x, y}, t)
-			// if boo {
-			// 	fmt.Println("Intersection:", max, maxY, x, y)
-			// }
+			if boo {
+				// fmt.Println("Intersection:", max, maxY, x, y)
+				if minX == 0 || x < minX {
+					minX = x
+				}
+				if minY == 0 || y < minY {
+					minY = y
+				}
+				if maxX == 0 || x > maxX {
+					maxX = x
+				}
+				if maxYY == 0 || y > maxYY {
+					maxYY = y
+				}
+			}
 			if boo && max > maxY {
 				maxY = max
 				maxPoint = Point{x, y}
 			}
 		}
 	}
+	// fmt.Println(minX, maxX, minY, maxYY)
 	return maxPoint, maxY
 }
 
