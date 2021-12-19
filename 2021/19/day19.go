9e5ea75c32f2e9c39dce2dd803a37e9b70a99cfc
Philip Corliss <pcorliss@50projects.com>
2021-12-19 13:23:41 -0600

    Day 19 init

---
+++ 2021/19/day19.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	// Load Input File
+	inputStr, err := os.ReadFile("19/input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Day 19")
+	fmt.Println("Part 01")
+	fmt.Println("Result: ", Part1(string(inputStr)))
+
+	fmt.Println("Part 02")
+	fmt.Println("Result: ", Part2(string(inputStr)))
+}
+
+func StringToNums(input string) []int {
+	nums := []int{}
+	lines := strings.Split(input, "\n")
+	for _, val := range lines {
+		n, err := strconv.Atoi(val)
+		if err != nil {
+			panic(err)
+		}
+		nums = append(nums, n)
+	}
+	return nums
+}
+
+func Part1(input string) int {
+	return 0
+}
+
+func Part2(input string) int {
+	return 0
+}
