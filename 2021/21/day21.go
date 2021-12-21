bbad6b7ae1feceebbf6ad44eb27caa6fa7371420
Philip Corliss <pcorliss@50projects.com>
2021-12-21 07:51:35 -0600

    Part 1 complete

--- 2021/21/day21.go
+++ 2021/21/day21.go
@@ -22,21 +22,56 @@ func main() {
 	fmt.Println("Result: ", Part2(string(inputStr)))
 }
 
-func StringToNums(input string) []int {
-	nums := []int{}
+type Player struct {
+	pos   int
+	score int
+}
+
+func StringToStarts(input string) []Player {
+	players := make([]Player, 2)
 	lines := strings.Split(input, "\n")
-	for _, val := range lines {
-		n, err := strconv.Atoi(val)
+	for i, line := range lines {
+		parts := strings.Split(line, " ")
+		n, err := strconv.Atoi(parts[4])
 		if err != nil {
 			panic(err)
 		}
-		nums = append(nums, n)
+		players[i].pos = n
 	}
-	return nums
+	return players
+}
+
+func PlayRounds(players []Player, rounds int) ([]Player, int) {
+	die := 0
+	rollCount := 0
+	for i := 0; i < rounds; i++ {
+		rollCount += 3
+		p := players[i%2] // This is a copy, maybe should be a reference
+		for j := 0; j < 3; j++ {
+			die = (die + 1) % 100
+			p.pos += die
+		}
+		for p.pos > 10 {
+			p.pos -= 10
+		}
+		p.score += p.pos
+		players[i%2] = p
+
+		if p.score >= 1000 {
+			break
+		}
+	}
+	return players, rollCount
 }
 
 func Part1(input string) int {
-	return 0
+	players := StringToStarts(input)
+	p, rolls := PlayRounds(players, 332)
+	loser := p[0]
+	if loser.score == 1000 {
+		loser = p[1]
+	}
+	return rolls * p[1].score
 }
 
 func Part2(input string) int {
--- 2021/21/day21_test.go
+++ 2021/21/day21_test.go
@@ -8,16 +8,48 @@ import (
 )
 
 var inputStr = strings.TrimSpace(`
-1721
-979
-366
-299
-675
-1456
+Player 1 starting position: 4
+Player 2 starting position: 8
 `)
 
+func TestStringToStarts(t *testing.T) {
+	players := StringToStarts(inputStr)
+	assert.Equal(t, 4, players[0].pos, "they should be equal")
+	assert.Equal(t, 8, players[1].pos, "they should be equal")
+	assert.Equal(t, 0, players[0].score, "they should be equal")
+	assert.Equal(t, 0, players[1].score, "they should be equal")
+}
+
+func TestPlayRoundsUpdatesTheScoreAndPos(t *testing.T) {
+	players := StringToStarts(inputStr)
+	p, _ := PlayRounds(players, 1)
+	assert.Equal(t, 10, p[0].pos, "they should be equal")
+	assert.Equal(t, 8, p[1].pos, "they should be equal")
+	assert.Equal(t, 10, p[0].score, "they should be equal")
+	assert.Equal(t, 0, p[1].score, "they should be equal")
+}
+
+func TestPlayRoundsHandlesBothPlayers(t *testing.T) {
+	players := StringToStarts(inputStr)
+	p, _ := PlayRounds(players, 331)
+	assert.Equal(t, 10, p[0].pos, "they should be equal")
+	assert.Equal(t, 3, p[1].pos, "they should be equal")
+	assert.Equal(t, 1000, p[0].score, "they should be equal")
+	assert.Equal(t, 745, p[1].score, "they should be equal")
+}
+
+func TestPlayRoundsStopsOnWin(t *testing.T) {
+	players := StringToStarts(inputStr)
+	p, rolls := PlayRounds(players, 332)
+	assert.Equal(t, 10, p[0].pos, "they should be equal")
+	assert.Equal(t, 3, p[1].pos, "they should be equal")
+	assert.Equal(t, 1000, p[0].score, "they should be equal")
+	assert.Equal(t, 745, p[1].score, "they should be equal")
+	assert.Equal(t, 993, rolls, "they should be equal")
+}
+
 func TestPart1(t *testing.T) {
-	assert.Equal(t, 0, Part1(inputStr), "they should be equal")
+	assert.Equal(t, 739785, Part1(inputStr), "they should be equal")
 }
 
 func TestPart2(t *testing.T) {
