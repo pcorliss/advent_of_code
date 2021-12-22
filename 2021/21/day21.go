33315be37d6f7be8b1ed4944edfb7707761e2603
Philip Corliss <pcorliss@50projects.com>
2021-12-21 18:48:34 -0600

    Part 2 completed but ugly slow code (1.5s)

--- 2021/21/day21.go
+++ 2021/21/day21.go
@@ -64,6 +64,79 @@ func PlayRounds(players []Player, rounds int) ([]Player, int) {
 	return players, rollCount
 }
 
+var rollDist = [27]int{3, 4, 4, 4, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 8, 8, 8, 9}
+
+var rollMap = map[int]int{
+	3: 1,
+	4: 3,
+	5: 6,
+	6: 7,
+	7: 6,
+	8: 3,
+	9: 1,
+}
+
+func PlayQuantumRounds(players []Player, score int) (int, int) {
+	countA := 0
+	countB := 0
+	// for _, roll := range rollDist {
+	// 	p := players[0]
+	// 	p.pos += roll
+	// 	for p.pos > 10 {
+	// 		p.pos -= 10
+	// 	}
+	// 	p.score += p.pos
+	// 	if p.score >= score {
+	// 		countA++
+	// 	} else {
+	// 		for _, roll2 := range rollDist {
+	// 			p2 := players[1]
+	// 			p2.pos += roll2
+	// 			for p2.pos > 10 {
+	// 				p2.pos -= 10
+	// 			}
+	// 			p2.score += p2.pos
+	// 			if p2.score >= score {
+	// 				countB++
+	// 			} else {
+	// 				a, b := PlayQuantumRounds([]Player{p, p2}, score)
+	// 				countA += a
+	// 				countB += b
+	// 			}
+	// 		}
+	// 	}
+	// }
+	for roll, mult := range rollMap {
+		p := players[0]
+		p.pos += roll
+		for p.pos > 10 {
+			p.pos -= 10
+		}
+		p.score += p.pos
+		if p.score >= score {
+			countA += mult
+		} else {
+			for roll2, mult2 := range rollMap {
+				p2 := players[1]
+				p2.pos += roll2
+				for p2.pos > 10 {
+					p2.pos -= 10
+				}
+				p2.score += p2.pos
+				if p2.score >= score {
+					countB += mult * mult2
+				} else {
+					a, b := PlayQuantumRounds([]Player{p, p2}, score)
+					countA += a * mult * mult2
+					countB += b * mult * mult2
+				}
+			}
+		}
+	}
+
+	return countA, countB
+}
+
 func Part1(input string) int {
 	players := StringToStarts(input)
 	p, rolls := PlayRounds(players, 332)
@@ -75,5 +148,8 @@ func Part1(input string) int {
 }
 
 func Part2(input string) int {
-	return 0
+	players := StringToStarts(input)
+	a, b := PlayQuantumRounds(players, 21)
+	fmt.Println("A:", a, "B:", b)
+	return a
 }
--- 2021/21/day21_test.go
+++ 2021/21/day21_test.go
@@ -48,10 +48,24 @@ func TestPlayRoundsStopsOnWin(t *testing.T) {
 	assert.Equal(t, 993, rolls, "they should be equal")
 }
 
+func TestPlayQuantumRounds(t *testing.T) {
+	players := StringToStarts(inputStr)
+	scoreA, scoreB := PlayQuantumRounds(players, 1)
+	assert.Equal(t, 27, scoreA, "they should be equal")
+	assert.Equal(t, 0, scoreB, "they should be equal")
+}
+
+func TestPlayQuantumRounds2(t *testing.T) {
+	players := StringToStarts(inputStr)
+	scoreA, scoreB := PlayQuantumRounds(players, 2)
+	assert.Equal(t, 183, scoreA, "they should be equal")
+	assert.Equal(t, 156, scoreB, "they should be equal")
+}
+
 func TestPart1(t *testing.T) {
 	assert.Equal(t, 739785, Part1(inputStr), "they should be equal")
 }
 
 func TestPart2(t *testing.T) {
-	assert.Equal(t, 0, Part2(inputStr), "they should be equal")
+	assert.Equal(t, 444356092776315, Part2(inputStr), "they should be equal")
 }
