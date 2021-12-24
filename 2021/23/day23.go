03f65036b7a38e287ba3daa1733caeb322737867
Philip Corliss <pcorliss@50projects.com>
2021-12-23 21:08:42 -0600

    Day 23 Part 1 completed. But slow >7s

--- 2021/23/day23.go
+++ 2021/23/day23.go
@@ -3,6 +3,7 @@ package main
 import (
 	"container/heap"
 	"fmt"
+	"math"
 	"os"
 	"strings"
 )
@@ -74,88 +75,284 @@ type Point struct {
 	y int
 }
 
-var boardLookup = map[Point]int{
-	{1, 1}:  0,  // Hallway Far Left
-	{2, 1}:  1,  // Hallway Almost Far Left
-	{4, 1}:  2,  // Between A/B
-	{6, 1}:  3,  // Between B/C
-	{8, 1}:  4,  // Between C/D
-	{10, 1}: 5,  // Hallway Almost Far Right
-	{11, 1}: 6,  // Hallway Far Right
-	{3, 2}:  7,  // A Col
-	{5, 2}:  8,  // B Col
-	{7, 2}:  9,  // C Col
-	{9, 2}:  10, // D Col
-	{3, 3}:  11, // A Col
-	{5, 3}:  12, // B Col
-	{7, 3}:  13, // C Col
-	{9, 3}:  14, // D Col
-}
-
-var pointLookup = [15]Point{
-	{1, 1},
-	{2, 1},
-	{4, 1},
-	{6, 1},
-	{8, 1},
-	{10, 1},
-	{11, 1},
-	{3, 2},
-	{5, 2},
-	{7, 2},
-	{9, 2},
-	{3, 3},
-	{5, 3},
-	{7, 3},
-	{9, 3},
-}
-
-var runeLookup = map[string]rune{
-	"A": 65,
-	"B": 66,
-	"C": 67,
-	"D": 68,
-}
-
 type State struct {
-	energy int
-	board  [15]Lizard // Using an array for easier past board state tracking
+	energy  int
+	steps   int
+	lizards [8]Lizard
 }
 
 type Lizard struct {
-	id  int
 	typ rune
+	pos Point
+}
+
+var boardLookup = map[Point]rune{
+	{1, 1}:  rune(0),  // Hallway Far Left
+	{2, 1}:  rune(0),  // Hallway Almost Far Left
+	{4, 1}:  rune(0),  // Between A/B
+	{6, 1}:  rune(0),  // Between B/C
+	{8, 1}:  rune(0),  // Between C/D
+	{10, 1}: rune(0),  // Hallway Almost Far Right
+	{11, 1}: rune(0),  // Hallway Far Right
+	{3, 2}:  rune(65), // A Col
+	{5, 2}:  rune(66), // B Col
+	{7, 2}:  rune(67), // C Col
+	{9, 2}:  rune(68), // D Col
+	{3, 3}:  rune(65), // A Col
+	{5, 3}:  rune(66), // B Col
+	{7, 3}:  rune(67), // C Col
+	{9, 3}:  rune(68), // D Col
 }
 
 func StringToState(input string) State {
-	state := State{0, [15]Lizard{}}
+	state := State{0, 0, [8]Lizard{}}
 	charCount := make(map[rune]int)
+	idx := 0
 	for y, line := range strings.Split(input, "\n") {
 		for x, char := range strings.Split(line, "") {
-			idx, exists := boardLookup[Point{x, y}]
-			if exists && char != "." && char != "#" {
+			if char != "." && char != "#" && char != " " {
 				char := []rune(char)[0]
 				charCount[char]++
-				l := Lizard{charCount[char], char}
-				state.board[idx] = l
+				l := Lizard{char, Point{x, y}}
+				state.lizards[idx] = l
+				idx++
 			}
 		}
 	}
 	return state
 }
 
+func ValidEndState(state State) bool {
+	for _, lizard := range state.lizards {
+		if boardLookup[lizard.pos] != lizard.typ {
+			return false
+		}
+	}
+	return true
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func CalcDistance(beg, dest Point) int {
+	x := int(math.Abs(float64(dest.x - beg.x)))
+	y := beg.y - 1 + dest.y - 1
+	return x + y
+}
+
 func ValidDest(state State, beg, dest Point) bool {
-	// Is it blocked?
-	// Can this Lizard go to this spot?
-	// Is this lizard allowed to move to this final spot?
-	// 2.0 will never move from the hallway into a room unless that room is their destination room
-	// 2.1 and that room contains no amphipods which do not also have that room as their own destination.
+	correctRune, exists := boardLookup[dest]
+	if !exists { // return false if the destination doesn't exist
+		return false
+	}
+
 	// 3.0 rom the hallway it will stay in that spot until it can move into a room
-	return false
+	if beg.y == 1 && dest.y == 1 {
+		return false
+	}
+	if beg.x == dest.x {
+		return false
+	}
+
+	bottomOfColumnFilled := false
+	bottomOfColumnCorrect := false
+	freeToMoveVertically := true
+	myLizard := Lizard{}
+	// Is it blocked?
+	// fmt.Println("Valid?:", beg, dest, state)
+	for _, liz := range state.lizards {
+		if dest == liz.pos {
+			return false
+		}
+
+		// fmt.Println("  Liz:", liz)
+		// Hallway Blocked
+		if liz.pos.y == 1 {
+			minX := min(beg.x, dest.x)
+			maxX := max(beg.x, dest.x)
+			// fmt.Println("Liz:", liz, beg, dest)
+			if liz.pos.x > minX && liz.pos.x < maxX {
+				return false
+			}
+		}
+		if !bottomOfColumnFilled && liz.pos.y == 3 && liz.pos.x == dest.x {
+			// fmt.Println("  Bottom Filled")
+			bottomOfColumnFilled = true
+		}
+
+		if liz.pos == beg {
+			// fmt.Println("MyLiz:", liz, beg)
+			myLizard = liz
+		}
+
+		if liz.pos.x == dest.x && liz.pos.y == 3 && liz.typ == correctRune {
+			bottomOfColumnCorrect = true
+		}
+
+		if liz.pos.x == beg.x && liz.pos.y < beg.y {
+			freeToMoveVertically = false
+		}
+	}
+
+	if beg.y == 3 && !freeToMoveVertically {
+		return false
+	}
+
+	// Is this lizard allowed to move to this final spot?
+	// fmt.Println("Correct:", correctRune, myLizard)
+	if correctRune != 0 && correctRune != myLizard.typ {
+		return false
+	}
+
+	if dest.y == 2 && !bottomOfColumnFilled {
+		// Must move to bottom of column if column empty
+		return false
+	}
+
+	if dest.y == 2 && !bottomOfColumnCorrect {
+		// that room contains no amphipods which do not also have that room as their own destination.
+		return false
+	}
+
+	// Can this Lizard go to this spot?
+	// 2.0 will never move from the hallway into a room unless that room is their destination room
+	return true
+}
+
+var energyLookup = map[rune]int{
+	rune(65): 1,    // A Col
+	rune(66): 10,   // B Col
+	rune(67): 100,  // C Col
+	rune(68): 1000, // D Col
+}
+
+func FindLowestEnergy(state State) int {
+	pq := make(PriorityQueue, 1)
+	pq[0] = &Item{
+		state:    state,
+		priority: 0,
+		index:    0,
+	}
+	heap.Init(&pq)
+
+	seen := make(map[[8]Lizard]bool)
+
+	for pq.Len() > 0 {
+		candidate := heap.Pop(&pq).(*Item)
+		if ValidEndState(candidate.state) {
+			return candidate.state.energy
+		}
+
+		if seen[candidate.state.lizards] {
+			continue
+		}
+		seen[candidate.state.lizards] = true
+
+		// if candidate.state.energy > 12521 {
+		// 	return 0
+		// }
+
+		// debug := false
+		// // if candidate.state.energy == 440 && candidate.state.steps == 2 {
+		// // if candidate.state.steps == 3 {
+		// if StateToString(candidate.state) == step1Str || StateToString(candidate.state) == step2Str || StateToString(candidate.state) == step3Str {
+		// 	fmt.Println("Candidates:", pq.Len())
+		// 	fmt.Println("Seen:", len(seen))
+		// 	fmt.Println("Steps:", candidate.state.steps)
+		// 	fmt.Println("Energy:", candidate.state.energy)
+		// 	fmt.Println(StateToString(candidate.state))
+		// 	debug = true
+		// }
+
+		// if pq.Len() == 27 {
+		// 	debug = true
+		// }
+
+		// if debug {
+		// 	fmt.Println("Heap Size:", pq.Len())
+		// 	fmt.Println("Candidate:", candidate.state.energy)
+		// 	fmt.Println(StateToString(candidate.state))
+		// 	fmt.Println("")
+		// }
+
+		for _, lizard := range candidate.state.lizards {
+			for dest, _ := range boardLookup {
+				if ValidDest(candidate.state, lizard.pos, dest) {
+					newLizards := [8]Lizard{}
+					newEnergy := candidate.state.energy
+					for i, l := range candidate.state.lizards {
+						if l == lizard {
+							newLizards[i] = Lizard{l.typ, dest}
+							newEnergy += CalcDistance(lizard.pos, dest) * energyLookup[l.typ]
+							// if debug && string(lizard.typ) == "C" {
+							// 	fmt.Println("Pos:", l, lizard.pos, dest)
+							// 	fmt.Println("Dist:", CalcDistance(lizard.pos, dest))
+							// 	fmt.Println("NewEnergy:", newEnergy)
+							// }
+						} else {
+							newLizards[i] = Lizard{l.typ, Point{l.pos.x, l.pos.y}}
+						}
+					}
+					if newEnergy == 0 {
+						panic("Invalid New Energy")
+					}
+					// if seen[newLizards] {
+					// 	continue
+					// }
+					newState := State{newEnergy, candidate.state.steps + 1, newLizards}
+					item := &Item{
+						state:    newState,
+						priority: newEnergy,
+					}
+					// if debug {
+					// 	fmt.Println("Pushing New Candidate")
+					// 	fmt.Println("Candidate:", newEnergy)
+					// 	fmt.Println(StateToString(newState))
+					// 	fmt.Println("")
+					// }
+					heap.Push(&pq, item)
+				}
+			}
+		}
+	}
+
+	return 0
+}
+
+func StateToString(state State) string {
+	lizards := make(map[Point]rune)
+	for _, l := range state.lizards {
+		lizards[l.pos] = l.typ
+	}
+	out := []string{}
+	for y := 1; y < 4; y++ {
+		line := ""
+		for x := 0; x < 12; x++ {
+			if r, exists := lizards[Point{x, y}]; exists {
+				line += string(r)
+			} else {
+				line += "."
+			}
+		}
+		out = append(out, line)
+	}
+	return strings.Join(out, "\n")
 }
 
 func Part1(input string) int {
-	return 0
+	state := StringToState(input)
+	return FindLowestEnergy(state)
 }
 
 func Part2(input string) int {
--- 2021/23/day23_test.go
+++ 2021/23/day23_test.go
@@ -19,12 +19,151 @@ func TestStringToState(t *testing.T) {
 	state := StringToState(inputStr)
 	// fmt.Println("Board:", state)
 	assert.Equal(t, 0, state.energy, "they should be equal")
-	assert.Equal(t, rune(65), state.board[14].typ, "they should be equal")
-	assert.Equal(t, 2, state.board[14].id, "they should be equal")
+	assert.Equal(t, rune(65), state.lizards[7].typ, "they should be equal")
+	assert.Equal(t, Point{9, 3}, state.lizards[7].pos, "they should be equal")
 }
 
+var endState = strings.TrimSpace(`
+#############
+#...........#
+###A#B#C#D###
+  #A#B#C#D#
+  #########
+`)
+
+func TestValidEndState(t *testing.T) {
+	state := StringToState(endState)
+	assert.Equal(t, true, ValidEndState(state), "they should be equal")
+}
+func TestInValidEndState(t *testing.T) {
+	state := StringToState(inputStr)
+	assert.Equal(t, false, ValidEndState(state), "they should be equal")
+}
+
+var blockedStr = strings.TrimSpace(`
+#############
+#.A...C.....#
+###.#.#B#D###
+  #A#B#C#D#
+  #########
+`)
+
+var colBlockedStr = strings.TrimSpace(`
+#############
+#AA.......DD#
+###.#.#B#.###
+  #.#B#C#C#
+  #########
+`)
+
+var validDestTest = []struct {
+	input       string
+	beg         Point
+	dest        Point
+	expected    bool
+	description string
+}{
+	{inputStr, Point{7, 2}, Point{0, 0}, false, "Invalid Point"},
+	{inputStr, Point{7, 2}, Point{4, 1}, true, "Hallway is valid"},
+	{inputStr, Point{7, 2}, Point{3, 2}, false, "Already Filled"},
+	{blockedStr, Point{7, 2}, Point{5, 2}, false, "Hallway is blocked"},
+	{blockedStr, Point{2, 1}, Point{3, 2}, true, "Hallway is not blocked"},
+	{colBlockedStr, Point{2, 1}, Point{3, 2}, false, "Must move to bottom of column"},
+	{colBlockedStr, Point{5, 3}, Point{3, 3}, false, "Must move to the correct column for that type"},
+	{colBlockedStr, Point{10, 1}, Point{9, 2}, false, "Won't block in the wrong type of lizard"},
+	{colBlockedStr, Point{2, 1}, Point{4, 1}, false, "Won't move around the hallway"},
+	{colBlockedStr, Point{7, 3}, Point{4, 1}, false, "Can't move if blocked vertically"},
+	{colBlockedStr, Point{5, 3}, Point{4, 1}, true, "Can move if not blocked vertically"},
+	{colBlockedStr, Point{5, 3}, Point{5, 2}, false, "Can't move one space up and leave a gap"},
+}
+
+func TestValidDest(t *testing.T) {
+	for _, tt := range validDestTest {
+		t.Run(tt.description, func(t *testing.T) {
+			state := StringToState(tt.input)
+			actual := ValidDest(state, tt.beg, tt.dest)
+			if actual != tt.expected {
+				t.Errorf("ValidDest(%v, %v) got %t want %t", tt.beg, tt.dest, actual, tt.expected)
+			}
+		})
+	}
+}
+
+var expectedStr = strings.TrimSpace(`
+............
+...B.C.B.D..
+...A.D.C.A..
+`)
+
+// Wrong Energy
+// Candidates: 2646
+// Seen: 286
+// Steps: 2
+// Energy: 240
+// ........B...
+// ...B...C.D..
+// ...A.D.C.A..
+
+func TestStateToString(t *testing.T) {
+	state := StringToState(inputStr)
+	assert.Equal(t, expectedStr, StateToString(state), "they should be equal")
+}
+
+var calcDistTest = []struct {
+	beg         Point
+	dest        Point
+	expected    int
+	description string
+}{
+	{Point{3, 2}, Point{4, 1}, 2, "Calcs simple up and right"},
+	{Point{3, 2}, Point{5, 3}, 5, "Calcs up and down"},
+	{Point{5, 2}, Point{7, 2}, 4, "Calcs up and down mid"},
+}
+
+func TestCalcDistance(t *testing.T) {
+	for _, tt := range calcDistTest {
+		t.Run(tt.description, func(t *testing.T) {
+			actual := CalcDistance(tt.beg, tt.dest)
+			if actual != tt.expected {
+				t.Errorf("CalcDistance(%v, %v) got %d want %d", tt.beg, tt.dest, actual, tt.expected)
+			}
+		})
+	}
+}
+
+var step1Str = strings.TrimSpace(`
+....B.......
+...B.C...D..
+...A.D.C.A..
+`)
+
+var step2Str = strings.TrimSpace(`
+....B.......
+...B...C.D..
+...A.D.C.A..
+`)
+
+var step3Str = strings.TrimSpace(`
+....B.D.....
+...B...C.D..
+...A...C.A..
+`)
+
+// Candidates: 2959
+// Seen: 523
+// Steps: 2
+// Energy: 480
+// ....B.......
+// ...B...C.D..
+// ...A.D.C.A..
+
+// func TestFindLowestEnergy(t *testing.T) {
+// 	state := StringToState(inputStr)
+// 	assert.Equal(t, 12521, FindLowestEnergy(state), "they should be equal")
+// }
+
 func TestPart1(t *testing.T) {
-	assert.Equal(t, 0, Part1(inputStr), "they should be equal")
+	assert.Equal(t, 12521, Part1(inputStr), "they should be equal")
 }
 
 func TestPart2(t *testing.T) {
