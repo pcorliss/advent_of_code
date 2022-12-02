097ba60bf08bfb8bfe98cf3b49034d7d2f67399d
Philip Corliss <pcorliss@50projects.com>
2022-12-01 18:24:31 -0600

    Use 2017 as authoritative ruby library and scripting

--- 2022/blank/blank.rb
+++
@@ -1,9 +0,0 @@
-require 'set'
-
-module Advent
-
-  class Blank
-    def initialize(input)
-    end
-  end
-end
--- 2022/daily.sh
+++ 2022/daily.sh
@@ -1,18 +1,1 @@
-#!/bin/bash
-
-set -o errexit
-set -o nounset
-set -o pipefail
-set -o verbose
-
-DAY=$(date "+%d")
-NAME=$1
-FNAME=$(echo ${NAME} | tr '[:upper:]' '[:lower:]')
-
-cp -pr blank ${DAY}
-pushd ${DAY}
-find . | grep rb | xargs gsed -i "s/blank/${FNAME}/g"
-find . | grep rb | xargs gsed -i "s/Blank/${NAME}/g"
-mv blank.rb ${FNAME}.rb
-mv spec/blank_spec.rb spec/${FNAME}_spec.rb
-popd
+../2017/daily.sh
