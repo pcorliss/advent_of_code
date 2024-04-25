5f704efb70da7ce6fdc52c5cd60acde3c27c2d81
Philip Corliss <pcorliss@50projects.com>
2024-04-25 13:46:26 -0500

    Adding yjit optimization for performance for next year.

---
+++ 2024/blank/blank.rb
@@ -0,0 +1,18 @@
+require '../lib/yjit.rb'
+require '../lib/grid.rb'
+require '../lib/ring.rb'
+
+module Advent
+
+  class Blank
+    attr_accessor :debug
+
+    def initialize(input)
+      @debug = false
+    end
+
+    def debug!
+      @debug = true
+    end
+  end
+end
