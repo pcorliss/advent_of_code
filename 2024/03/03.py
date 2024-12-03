3fb6c3412275fb0d2af178b81ac4c06304b04249
Philip Corliss <pcorliss@50projects.com>
2024-12-03 12:58:52 -0600

    2024 Day 3 Part 2 Complete

--- 2024/03/03.py
+++ 2024/03/03.py
@@ -15,17 +15,39 @@ def part1(input_text):
   sum = 0
   for line in data:
     matches = re.findall(r"mul\((\d+),(\d+)\)", line)
-    print(f"Line: {line} matches: {matches}")
+    # print(f"Line: {line} matches: {matches}")
     for match in matches:
-      mul = int(match[0]) * int(match[1])
+      mul_a, mul_b = match
+      mul = int(mul_a) * int(mul_b)
       sum += mul
 
   return sum
 
+def part2(input_text):
+  data = parse(input_text)
+  sum = 0
+  multiply = True
+  for line in data:
+    matches = re.findall(r"(do\(\))|(don't\(\))|mul\((\d+),(\d+)\)", line)
+    # print(f"Line: {line} matches: {matches}")
+    for match in matches:
+      do, dont, mul_a, mul_b = match
+      if do:
+        multiply = True
+      elif dont:
+        multiply = False
+      elif multiply and mul_a and mul_b:
+        mul = int(mul_a) * int(mul_b)
+        sum += mul
 
+  return sum
 
 sample = """
 xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
+"""
+
+sample2 = """
+xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
 """
 
 print(part1(sample))
@@ -33,7 +55,7 @@ if len(sys.argv) >= 2:
   with open(sys.argv[1], 'r') as file:
     print(part1(file.read()))
 
-# print(part2(sample))
-# if len(sys.argv) >= 2:
-#   with open(sys.argv[1], 'r') as file:
-#     print(part2(file.read()))
+print(part2(sample2))
+if len(sys.argv) >= 2:
+  with open(sys.argv[1], 'r') as file:
+    print(part2(file.read()))
