61a0428b4228c073f9cad9b43fc35f58c88a3cb2
Philip Corliss <pcorliss@50projects.com>
2024-12-07 10:28:16 -0600

    2024 Day 7 Part 1 Completed

---
+++ 2024/07/daily.py
@@ -0,0 +1,50 @@
+import sys
+import itertools
+
+def parse(input_text):
+  out = {}
+  for line in input_text.strip().split('\n'):
+    if len(line) == 0:
+      continue
+    goal, n = line.split(': ')
+    nums = n.split(' ')
+    out[int(goal)] = list(map(int, nums))
+  return out
+
+def add_symbols(goal, nums, symbols):
+  for symbols in itertools.product(['*', '+'], repeat=len(nums) - 1):
+    equation = [nums[0]]
+    sum = nums[0]
+    for i in range(1, len(nums)):
+      equation.append(symbols[i - 1])
+      equation.append(nums[i])
+      if symbols[i - 1] == '*':
+        sum *= nums[i]
+      elif symbols[i - 1] == '+':
+        sum += nums[i]
+      else:
+        raise f"Unknown symbol {equation}"
+
+    if goal == sum:
+      return equation
+    
+  return None
+
+def part1(input_text):
+  data = parse(input_text)
+  sum = 0
+  for goal, nums in data.items():
+    equation = add_symbols(goal, nums, ['*', '+'])
+    if equation:
+      sum += goal
+  return sum
+
+def part2(input_text):
+  return 0
+
+if __name__ == "__main__":
+  with open(__file__.rsplit('/', 1)[0] + "/input.txt", 'r') as file:
+    print(part1(file.read()))
+
+  with open(__file__.rsplit('/', 1)[0] + "/input.txt", 'r') as file:
+    print(part2(file.read()))
---
+++ 2024/07/daily_test.py
@@ -0,0 +1,47 @@
+import pytest
+from daily import *
+
+@pytest.fixture
+def sample_data():
+    return """
+190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20
+"""
+
+def test_parse(sample_data):
+    parsed_data = parse(sample_data)
+
+    assert len(parsed_data) == 9
+    assert parsed_data[192] == [17, 8, 14]
+
+def test_add_symbols_one_pos(sample_data):
+    symbolized_data = add_symbols(190, [10, 19], ['+', '*'])
+
+    assert symbolized_data == [10, '*', 19]
+
+def test_add_symbols_two_pos(sample_data):
+    symbolized_data = add_symbols(3267, [81,40,27], ['+', '*'])
+
+    # Symbols can be swapped so test may be unstable
+    assert symbolized_data == [81, '*', 40, '+', 27]
+
+def test_add_symbols_left_to_right(sample_data):
+    symbolized_data = add_symbols(292, [11,6,16,20], ['+', '*'])
+
+    # Symbols can be swapped so test may be unstable
+    assert symbolized_data == [11, '+', 6, '*', 16, '+', 20]
+
+def test_add_symbols_unsolvable(sample_data):
+    symbolized_data = add_symbols(83, [17,5], ['+', '*'])
+
+    assert symbolized_data is None
+
+def test_part1(sample_data):
+    assert part1(sample_data) == 3749
