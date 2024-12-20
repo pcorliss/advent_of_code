edd6b25c208a1ba68c09c103ad1721169fc9e3c5
Philip Corliss <pcorliss@50projects.com>
2024-12-20 12:50:39 -0600

    2024 Day 20 Part 1 Complete

---
+++ 2024/20/daily.py
@@ -0,0 +1,67 @@
+def parse(input_text):
+  grid = []
+  s = None
+  e = None
+
+  for line in input_text.strip().split('\n'):
+    if len(line) == 0:
+      continue
+
+    grid.append(list(line))
+
+  for y in range(len(grid)):
+    for x in range(len(grid[y])):
+      if grid[y][x] == 'S':
+        s = (x, y)
+        grid[y][x] = '.'
+      elif grid[y][x] == 'E':
+        e = (x, y)
+        grid[y][x] = '.'
+
+  return (grid, s, e)
+
+def fill_path(grid, s, e):
+  steps = {}
+  steps[s] = 0
+  pos = s
+
+  step_count = 0
+  while pos != e:
+    x, y = pos
+
+    for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+      if grid[y+dy][x+dx] == '.' and (x+dx, y+dy) not in steps:
+        pos = (x+dx, y+dy)
+        steps[pos] = steps[(x, y)] + 1
+        break
+
+  return steps
+
+def cheat_options(steps):
+  options = []
+  for pos, step in steps.items():
+    x, y = pos
+    for dx, dy in [(-2, 0), (2, 0), (0, -2), (0, 2)]:
+      nx, ny = x+dx, y+dy
+      if (nx, ny) in steps and steps[(nx, ny)] > step + 2:
+        options.append(steps[(nx, ny)] - 2 - step)
+
+  return options
+
+  
+def part1(input_text, cheat_threshold=100):
+  grid, s, e = parse(input_text)
+  steps = fill_path(grid, s, e)
+  options = cheat_options(steps)
+
+  return len([o for o in options if o >= cheat_threshold])
+
+def part2(input_text):
+  return 0
+
+if __name__ == "__main__":
+  with open(__file__.rsplit('/', 1)[0] + "/input.txt", 'r') as file:
+    print(part1(file.read()))
+
+  with open(__file__.rsplit('/', 1)[0] + "/input.txt", 'r') as file:
+    print(part2(file.read()))
---
+++ 2024/20/daily_test.py
@@ -0,0 +1,65 @@
+import pytest
+from daily import *
+
+@pytest.fixture
+def sample_data():
+  return """
+###############
+#...#...#.....#
+#.#.#.#.#.###.#
+#S#...#.#.#...#
+#######.#.#.###
+#######.#.#...#
+#######.#.###.#
+###..E#...#...#
+###.#######.###
+#...###...#...#
+#.#####.#.###.#
+#.#...#.#.#...#
+#.#.#.#.#.#.###
+#...#...#...###
+###############
+"""
+
+@pytest.fixture
+def parsed_data(sample_data):
+  return parse(sample_data)
+
+def test_parse(sample_data):
+  grid, s, e = parse(sample_data)
+  
+  assert len(grid) == 15
+  assert len(grid[0]) == 15
+
+  assert s == (1, 3)
+  assert e == (5, 7)
+
+  assert grid[3][1] == '.'
+  assert grid[7][5] == '.'
+
+
+def test_fill_path(parsed_data):
+  grid, s, e = parsed_data
+  steps = fill_path(grid, s, e)
+
+  assert steps[s] == 0
+  assert steps[e] == 84
+
+def test_cheat_options(parsed_data):
+  grid, s, e = parsed_data
+  steps = fill_path(grid, s, e)
+  options = cheat_options(steps)
+
+  assert len(options) == 44
+  assert 20 in options
+  assert 36 in options
+  assert 38 in options
+  assert 40 in options
+  assert 64 in options
+
+
+def test_part1(sample_data):
+  assert part1(sample_data, 36) == 4
+
+# def test_part2(sample_data):
+#   assert part2(sample_data) == 16
