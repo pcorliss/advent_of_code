5b49518e681d2e5896ff0952ebbdc7182e91b47f
Philip Corliss <pcorliss@50projects.com>
2025-12-07 10:28:46 -0600

    2025 Day 7 Part 2 Complete

--- 2025/lib/day07/day07.ex
+++ 2025/lib/day07/day07.ex
@@ -54,6 +54,33 @@ defmodule Day07 do
     end)
   end
 
+  def increment_map(m, keys, incr) do
+    Enum.reduce(keys, m, fn k, acc ->
+      {_, new_m} =
+        Map.get_and_update(acc, k, fn cur ->
+          if cur == nil do
+            {cur, incr}
+          else
+            {cur, cur + incr}
+          end
+        end)
+
+      new_m
+    end)
+  end
+
+  def quantum_split_row(splitters, beams, y) do
+    beams
+    |> Enum.reduce({0, %{}}, fn {{b_x, _}, b_c}, {count, new_beams} ->
+      if MapSet.member?(splitters, {b_x, y}) do
+        new_beams = increment_map(new_beams, [{b_x - 1, y}, {b_x + 1, y}], b_c)
+        {count + 1, new_beams}
+      else
+        {count, increment_map(new_beams, [{b_x, y}], b_c)}
+      end
+    end)
+  end
+
   def part1(infile) do
     {start, splitters, max_y} = input(infile)
 
@@ -67,8 +94,21 @@ defmodule Day07 do
   end
 
   def part2(infile) do
-    input(infile)
-    0
+    {start, splitters, max_y} = input(infile)
+
+    {_, new_beams} =
+      Enum.reduce(1..max_y, {0, %{start => 1}}, fn y, {count, beams} ->
+        {new_count, new_beams} = quantum_split_row(splitters, beams, y)
+
+        # if new_count > 0 do
+        #   IO.puts("Row: #{y} Splits: #{new_count}, Beams: #{inspect(new_beams)}")
+        # end
+
+        {count + new_count, new_beams}
+      end)
+
+    Map.values(new_beams)
+    |> Enum.sum()
   end
 
   def main do
--- 2025/test/day07/day07_test.exs
+++ 2025/test/day07/day07_test.exs
@@ -51,11 +51,50 @@ defmodule Day07Test do
     assert MapSet.to_list(new_beams) == [{6, 2}, {8, 2}]
   end
 
+  test "increment_map" do
+    a = Day07.increment_map(%{}, [:a, :b], 1)
+    assert a[:a] == 1
+    assert a[:b] == 1
+
+    b = Day07.increment_map(%{a: 2, b: 3}, [:a, :b], 1)
+    assert b[:a] == 3
+    assert b[:b] == 4
+  end
+
+  test "quantum_split_row", %{temp_file: temp_file} do
+    {start, splitters, _} = Day07.input(temp_file)
+    beams = %{start => 1}
+
+    {count, new_beams} = Day07.quantum_split_row(splitters, beams, 1)
+    assert count == 0
+    assert new_beams == %{{7, 1} => 1}
+
+    {count, new_beams} = Day07.quantum_split_row(splitters, new_beams, 2)
+    assert count == 1
+    assert new_beams == %{{6, 2} => 1, {8, 2} => 1}
+
+    {count, new_beams} = Day07.quantum_split_row(splitters, new_beams, 3)
+    assert count == 0
+    assert new_beams == %{{6, 3} => 1, {8, 3} => 1}
+
+    {count, new_beams} = Day07.quantum_split_row(splitters, new_beams, 4)
+    assert count == 2
+    assert new_beams == %{{5, 4} => 1, {7, 4} => 2, {9, 4} => 1}
+
+    {count, new_beams} = Day07.quantum_split_row(splitters, new_beams, 5)
+    assert count == 0
+    assert new_beams == %{{5, 5} => 1, {7, 5} => 2, {9, 5} => 1}
+
+    {count, new_beams} = Day07.quantum_split_row(splitters, new_beams, 6)
+    assert count == 3
+    assert new_beams == %{{4, 6} => 1, {6, 6} => 3, {8, 6} => 3, {10, 6} => 1}
+  end
+
   test "part1", %{temp_file: temp_file} do
     assert Day07.part1(temp_file) == 21
   end
 
   test "part2", %{temp_file: temp_file} do
-    assert Day07.part2(temp_file) == 0
+    assert Day07.part2(temp_file) == 40
   end
 end
