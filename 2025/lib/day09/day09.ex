1f86f078ab7876fd1f2000707dc7f2739013a0e7
Philip Corliss <pcorliss@50projects.com>
2025-12-09 20:46:00 -0600

    Far too slow for input

--- 2025/lib/day09/day09.ex
+++ 2025/lib/day09/day09.ex
@@ -7,7 +7,8 @@ defmodule Day09 do
       [a, b] = String.split(line, ",")
       {String.to_integer(a), String.to_integer(b)}
     end)
-    |> MapSet.new()
+
+    # |> MapSet.new()
   end
 
   def part1(infile) do
@@ -24,9 +25,130 @@ defmodule Day09 do
     |> Enum.max()
   end
 
+  def draw_border(points) do
+    # Produces a list of point pairs and wraps around
+    Enum.zip(points, tl(points) ++ [hd(points)])
+    |> Enum.reduce(MapSet.new(), fn {{a_x, a_y}, {b_x, b_y}}, border ->
+      edge =
+        cond do
+          a_x == b_x ->
+            y_step = if(a_y <= b_y, do: 1, else: -1)
+            for y <- a_y..b_y//y_step, do: {a_x, y}
+
+          a_y == b_y ->
+            x_step = if(a_x <= b_x, do: 1, else: -1)
+            for x <- a_x..b_x//x_step, do: {x, a_y}
+        end
+
+      MapSet.union(border, MapSet.new(edge))
+    end)
+  end
+
+  def border_min_max(points) do
+    x_p = Enum.map(points, fn {x, _} -> x end)
+    y_p = Enum.map(points, fn {_, y} -> y end)
+
+    {Enum.min(x_p), Enum.max(x_p), Enum.min(y_p), Enum.max(y_p)}
+  end
+
+  def outside(border) do
+    {min_x, max_x} = border |> Enum.map(&elem(&1, 0)) |> Enum.min_max()
+    {min_y, max_y} = border |> Enum.map(&elem(&1, 1)) |> Enum.min_max()
+
+    min_x = min_x - 1
+    max_x = max_x + 1
+    min_y = min_y - 1
+    max_y = max_y + 1
+
+    start = {min_x, min_y}
+    # start = {7, 0}
+
+    neighbors = fn {x, y} ->
+      [
+        {x + 1, y},
+        {x - 1, y},
+        {x, y + 1},
+        {x, y - 1}
+      ]
+      |> Enum.filter(fn {nx, ny} ->
+        nx >= min_x and nx <= max_x and ny >= min_y and ny <= max_y
+      end)
+      |> Enum.reject(&MapSet.member?(border, &1))
+    end
+
+    bfs = fn bfs, frontier, visited ->
+      case frontier do
+        [] ->
+          visited
+
+        _ ->
+          {new_visited, new_frontier} =
+            Enum.reduce(frontier, {visited, []}, fn p, {v, f} ->
+              if MapSet.member?(v, p) do
+                {v, f}
+              else
+                ns = neighbors.(p)
+                {MapSet.put(v, p), ns ++ f}
+              end
+            end)
+
+          bfs.(bfs, new_frontier, new_visited)
+      end
+    end
+
+    bfs.(bfs, [start], MapSet.new())
+  end
+
+  def rects(points) do
+    for {{a_x, a_y}, i} <- Enum.with_index(points),
+        {{b_x, b_y}, j} <- Enum.with_index(points),
+        j > i,
+        into: [] do
+      {{a_x, a_y}, {b_x, b_y}, (abs(a_x - b_x) + 1) * (abs(a_y - b_y) + 1)}
+    end
+  end
+
+  def rect_edge_points({a_x, a_y}, {b_x, b_y}) do
+    x1 = min(a_x, b_x)
+    x2 = max(a_x, b_x)
+    y1 = min(a_y, b_y)
+    y2 = max(a_y, b_y)
+
+    top = for x <- x1..x2, do: {x, y1}
+    bottom = for x <- x1..x2, do: {x, y2}
+    left = for y <- y1..y2, do: {x1, y}
+    right = for y <- y1..y2, do: {x2, y}
+
+    MapSet.new(top ++ bottom ++ left ++ right)
+  end
+
   def part2(infile) do
-    input(infile)
-    0
+    points = input(infile)
+    rects = rects(points)
+    IO.puts("Generated Rects #{length(rects)}")
+
+    border = Day09.draw_border(points)
+    IO.puts("Generated border #{MapSet.size(border)}")
+
+    outside = Day09.outside(border)
+    IO.puts("Generated Outside Border #{MapSet.size(outside)}")
+
+    {{_, _, area}, _} =
+      Enum.sort_by(rects, fn {_, _, area} -> area end, :desc)
+      |> Enum.with_index()
+      |> Enum.find(fn {{a, b, _}, idx} ->
+        if rem(idx, 1000) == 0 do
+          IO.puts("Considered #{idx} rects")
+        end
+
+        touches_outside =
+          rect_edge_points(a, b)
+          |> Enum.any?(&MapSet.member?(outside, &1))
+
+        not touches_outside
+      end)
+
+    area
   end
 
   def main do
--- 2025/test/day09/day09_test.exs
+++ 2025/test/day09/day09_test.exs
@@ -22,15 +22,69 @@ defmodule Day09Test do
 
   test "input", %{temp_file: temp_file} do
     result = Day09.input(temp_file)
-    assert MapSet.size(result) == 8
-    assert MapSet.member?(result, {7, 1})
+    assert length(result) == 8
+    assert Enum.member?(result, {7, 1})
   end
 
   test "part1", %{temp_file: temp_file} do
     assert Day09.part1(temp_file) == 50
   end
 
+  test "draw_border", %{temp_file: temp_file} do
+    points = Day09.input(temp_file)
+    border = Day09.draw_border(points)
+
+    # Includes the start point
+    assert MapSet.member?(border, {7, 1})
+    # X Dir
+    assert MapSet.member?(border, {8, 1})
+    # Y Dir with wrap-around
+    assert MapSet.member?(border, {7, 2})
+
+    # Not Border
+    assert not MapSet.member?(border, {8, 0})
+    assert not MapSet.member?(border, {8, 2})
+  end
+
+  test "border_min_max", %{temp_file: temp_file} do
+    points = Day09.input(temp_file)
+    {x_min, x_max, y_min, y_max} = Day09.border_min_max(points)
+
+    assert x_min == 2
+    assert x_max == 11
+    assert y_min == 1
+    assert y_max == 7
+  end
+
+  test "outside", %{temp_file: temp_file} do
+    points = Day09.input(temp_file)
+    border = Day09.draw_border(points)
+
+    outside = Day09.outside(border)
+
+    assert MapSet.member?(outside, {7, 0})
+    assert MapSet.member?(outside, {8, 0})
+    assert MapSet.member?(outside, {6, 1})
+    assert MapSet.member?(outside, {6, 2})
+
+    assert not MapSet.member?(outside, {7, 1})
+  end
+
+  test "rects", %{temp_file: temp_file} do
+    points = Day09.input(temp_file)
+    r = Day09.rects(points)
+
+    assert length(r) == 28
+
+    max_rect =
+      Enum.max_by(r, fn {_, _, area} ->
+        area
+      end)
+
+    assert max_rect == {{11, 1}, {2, 5}, 50}
+  end
+
   test "part2", %{temp_file: temp_file} do
-    assert Day09.part2(temp_file) == 0
+    assert Day09.part2(temp_file) == 24
   end
 end
