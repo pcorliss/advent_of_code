1106a691cc87ac362802fbea5609ae9a3413df4d
Philip Corliss <pcorliss@50projects.com>
2025-12-11 19:40:36 -0600

    2025 Day 11 Part 2 Complete - Hacky solution that is too slow to compute one leg of the paths

--- 2025/lib/day11/day11.ex
+++ 2025/lib/day11/day11.ex
@@ -9,16 +9,7 @@ defmodule Day11 do
     end)
   end
 
-  # graph: %{node => [neighbors]}
-  # start: starting node
-  # target: destination node
-  #
-  # Returns: list of paths, each path is a list of nodes
-  # def all_paths(graph, start, target) do
-  #   dfs(graph, start, target, [], [])
-  # end
-
-  def dfs(graph, node, target, path, results) do
+  def dfs(graph, node, target, path, results, filter \\ "") do
     path = [node | path]
 
     cond do
@@ -30,13 +21,59 @@ defmodule Day11 do
         neighbors = Map.get(graph, node, [])
 
         Enum.reduce(neighbors, results, fn neighbor, acc ->
-          if neighbor in path do
+          if neighbor in path or neighbor == filter do
             # Avoid cycles — skip nodes already on the current path
             acc
           else
-            dfs(graph, neighbor, target, path, acc)
+            dfs(graph, neighbor, target, path, acc, filter)
           end
         end)
+    end
+  end
+
+  def all_paths(graph, start, target, filter \\ "") do
+    {paths, _} = dfs_2(graph, start, target, MapSet.new(), %{}, filter)
+    paths
+  end
+
+  defp dfs_2(graph, node, target, visited, memo, filter) do
+    cond do
+      # Target reached
+      node == target ->
+        {[[target]], memo}
+
+      # Cycle detected
+      node in visited ->
+        {[], memo}
+
+      # filter detected
+      node == filter ->
+        {[], memo}
+
+      # Cached result
+      Map.has_key?(memo, node) ->
+        {memo[node], memo}
+
+      true ->
+        neighbors = Map.get(graph, node, [])
+        visited = MapSet.put(visited, node)
+
+        {paths, memo} =
+          Enum.reduce(neighbors, {[], memo}, fn neigh, {acc_paths, acc_memo} ->
+            {child_paths, new_memo} = dfs_2(graph, neigh, target, visited, acc_memo, filter)
+
+            # Prepend current node to each child path
+            prefixed =
+              for path <- child_paths do
+                [node | path]
+              end
+
+            {acc_paths ++ prefixed, new_memo}
+          end)
+
+        # Store in memo
+        memo = Map.put(memo, node, paths)
+        {paths, memo}
     end
   end
 
@@ -45,9 +82,74 @@ defmodule Day11 do
     length(dfs(graph, "you", "out", [], []))
   end
 
+  def reverse_graph(graph) do
+    graph
+    |> Enum.reduce(%{}, fn {node, neighbors}, acc ->
+      Enum.reduce(neighbors, acc, fn neigh, acc2 ->
+        Map.update(acc2, neigh, [node], fn list -> [node | list] end)
+      end)
+    end)
+  end
+
+  def find_paths(graph) do
+    rev_graph = reverse_graph(graph)
+
+    # Slow
+    # svr_fft = dfs(graph, "svr", "fft", [], [], "dac")
+    # IO.puts("svr->fft: #{length(svr_fft)}")
+
+    svr_fft = all_paths(graph, "svr", "fft", "dac")
+    IO.puts("svr->fft: #{length(svr_fft)}")
+
+    # Fast
+    svr_fft = dfs(rev_graph, "fft", "svr", [], [], "dac")
+    IO.puts("svr->fft: #{length(svr_fft)}")
+
+    # Slow
+    # svr_dac = dfs(graph, "svr", "dac", [], [], "fft")
+    # IO.puts("svr->dac: #{length(svr_dac)}")
+
+    # Alsp Slow
+    # svr_dac = all_paths(graph, "svr", "dac", "fft")
+    # IO.puts("svr->dac: #{length(svr_dac)}")
+
+    # Also slow
+    # svr_dac = dfs(rev_graph, "dac", "svr", [], [], "fft")
+    # IO.puts("svr->dac: #{length(svr_dac)}")
+
+    # Slow
+    # fft_dac = dfs(graph, "fft", "dac", [], [])
+    # IO.puts("fft->dac: #{length(fft_dac)}")
+
+    # Also Slow
+    # fft_dac = dfs(rev_graph, "dac", "fft", [], [])
+    # IO.puts("fft->dac: #{length(fft_dac)}")
+
+    fft_dac = all_paths(graph, "fft", "dac")
+    IO.puts("fft->dac: #{length(fft_dac)}")
+
+    # Fast
+    dac_fft = dfs(graph, "dac", "fft", [], [])
+    IO.puts("dac->fft: #{length(dac_fft)}")
+
+    # Slow
+    # fft_out = dfs(graph, "fft", "out", [], [], "dac")
+    # IO.puts("fft->out: #{length(fft_out)}")
+
+    # Fast
+    dac_out = dfs(graph, "dac", "out", [], [], "fft")
+    IO.puts("dac->out: #{length(dac_out)}")
+
+    length(svr_fft) * length(fft_dac) * length(dac_out)
+    # +
+    # length(svr_dac) * length(dac_fft) * length(fft_out)
+
+    # 0
+  end
+
   def part2(infile) do
-    input(infile)
-    0
+    graph = input(infile)
+    find_paths(graph)
   end
 
   def main do
--- 2025/test/day11/day11_test.exs
+++ 2025/test/day11/day11_test.exs
@@ -1,46 +1,80 @@
 defmodule Day11Test do
   use ExUnit.Case, async: true
 
-  setup do
-    # Create a temporary file with sample data
-    content = ~S"""
-    aaa: you hhh
-    you: bbb ccc
-    bbb: ddd eee
-    ccc: ddd eee fff
-    ddd: ggg
-    eee: out
-    fff: out
-    ggg: out
-    hhh: ccc fff iii
-    iii: out
-    """
+  describe "part 1" do
+    setup do
+      # Create a temporary file with sample data
+      content = ~S"""
+      aaa: you hhh
+      you: bbb ccc
+      bbb: ddd eee
+      ccc: ddd eee fff
+      ddd: ggg
+      eee: out
+      fff: out
+      ggg: out
+      hhh: ccc fff iii
+      iii: out
+      """
 
-    {:ok, temp_file} = Briefly.create()
-    File.write!(temp_file, content)
+      {:ok, temp_file} = Briefly.create()
+      File.write!(temp_file, content)
 
-    {:ok, temp_file: temp_file}
+      {:ok, temp_file: temp_file}
+    end
+
+    test "input", %{temp_file: temp_file} do
+      result = Day11.input(temp_file)
+      assert map_size(result) == 10
+      assert result["you"] == ["bbb", "ccc"]
+      assert result["ccc"] == ["ddd", "eee", "fff"]
+    end
+
+    test "dfs", %{temp_file: temp_file} do
+      graph = Day11.input(temp_file)
+      paths = Day11.dfs(graph, "you", "out", [], [])
+      assert length(paths) == 5
+      assert Enum.member?(paths, ["you", "ccc", "fff", "out"])
+    end
+
+    test "part1", %{temp_file: temp_file} do
+      assert Day11.part1(temp_file) == 5
+    end
   end
 
-  test "input", %{temp_file: temp_file} do
-    result = Day11.input(temp_file)
-    assert map_size(result) == 10
-    assert result["you"] == ["bbb", "ccc"]
-    assert result["ccc"] == ["ddd", "eee", "fff"]
-  end
+  describe "part 2" do
+    setup do
+      # Create a temporary file with sample data
+      content = ~S"""
+      svr: aaa bbb
+      aaa: fft
+      fft: ccc
+      bbb: tty
+      tty: ccc
+      ccc: ddd eee
+      ddd: hub
+      hub: fff
+      eee: dac
+      dac: fff
+      fff: ggg hhh
+      ggg: out
+      hhh: out
+      """
 
-  test "dfs", %{temp_file: temp_file} do
-    graph = Day11.input(temp_file)
-    paths = Day11.dfs(graph, "you", "out", [], [])
-    assert length(paths) == 5
-    assert Enum.member?(paths, ["you", "ccc", "fff", "out"])
-  end
+      {:ok, temp_file} = Briefly.create()
+      File.write!(temp_file, content)
 
-  test "part1", %{temp_file: temp_file} do
-    assert Day11.part1(temp_file) == 5
-  end
+      {:ok, temp_file: temp_file}
+    end
 
-  test "part2", %{temp_file: temp_file} do
-    assert Day11.part2(temp_file) == 0
+    test "find_paths", %{temp_file: temp_file} do
+      graph = Day11.input(temp_file)
+      path_count = Day11.find_paths(graph)
+      assert path_count == 2
+    end
+
+    test "part2", %{temp_file: temp_file} do
+      assert Day11.part2(temp_file) == 2
+    end
   end
 end
